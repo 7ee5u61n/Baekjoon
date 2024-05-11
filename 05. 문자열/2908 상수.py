d853809c12e2d116b26de581816e5979c101a76f
7ee5u61n <le2471@gmail.com>
2024-05-11 21:09:03 +0900

    2024-05-11

--- 05. 문자열/2908 상수.py
+++ 05. 문자열/2908 상수.py
@@ -1,2 +1,6 @@
-A, B = map(int, input().split()) # 0이 포함되어 있지 않고 같지 않은 두개의 세자리 수
-w
+A, B = list(map(str, input().split())) # 0이 포함되어 있지 않고 같지 않은 두개의 세자리 수
+
+if int(A[::-1]) > int(B[::-1]):
+    print(A[::-1])
+else:
+    print(B[::-1])
