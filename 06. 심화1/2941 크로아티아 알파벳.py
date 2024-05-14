4c64c174dd5876604d8ed6e552fb6f29b58a25e1
7ee5u61n <le2471@gmail.com>
2024-05-14 12:15:14 +0900

    2024-05-14

--- 06. 심화1/2941 크로아티아 알파벳.py
+++ 06. 심화1/2941 크로아티아 알파벳.py
@@ -1,10 +1,26 @@
-croatian = str(input())
-dic = {}
+# 크로아티아 알파벳
+# ć 	c-
+# dž 	dz=
+# đ 	d-
+# lj 	lj
+# nj 	nj
+# š 	s=
+# ž     z=
 
-for chr in croatian:
-    if chr in dic:
-        dic[chr] += 1
+croatian = list(str(input())) # 알파벳 소문자와 -, = 입력가능
+count = 0
+
+for i in range(len(croatian)-1):
+    i += 0
+    if croatian[i] == 'd' and croatian[i+1] == 'z':
+        count = count + 1
+    elif croatian[i] == 'l' and croatian[i+1] == 'j':
+        count = count + 1
+    elif croatian[i] == 'n' and croatian[i+1] == 'j':
+        count = count + 1
+    elif croatian[i] == '-' or croatian[i] == '=':
+        continue
     else:
-        dic[chr] = 1
+        count = count + 1
 
-print(dic)
+print(count)
