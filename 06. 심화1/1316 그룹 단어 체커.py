8a8330488db40c6378a946b2b009cbfeee39bc7f
7ee5u61n <le2471@gmail.com>
2024-05-16 10:17:50 +0900

    2024-05-16

--- 06. 심화1/1316 그룹 단어 체커.py
+++ 06. 심화1/1316 그룹 단어 체커.py
@@ -2,17 +2,17 @@
 # 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만,
 # aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
 
-# 단어에서 문자의 연속이 끝나면 금지 리스트에 집어 넣고 금지 리스트에 있는 문자가 다시 나오면 그룹 단어가 아니게 만들기
 
 N = int(input()) # 단어의 개수
 
-nonGroupWord = 0
+nonGroupWord = 0 # 그룹단어가 아닌 개수
 
 for _ in range(N):
     word = (str(input()))
-    banLetter = [] # 반복이 끝나 더이상 나오면 안되는 단어
-    for i in range(len(word)):
-        i += 0
-        banLetter.append(word[i]) # 앞으로 나오지 말아야 할 문자 저장하는 리스트
+    for i in range(len(word)-1):
+        if word.find(word[i]) > word.find(word[i+1]): 
+            # word[i]가 처음 나타나는 위치가 word[i+1] 문자가 나타는 인덱스 보다 작다면 같은 문자가 연속되지 않음을 의미하므로 그룹단어가 아니다.
+            nonGroupWord += 1
+            break
 
-print(len(word))
+print(N - nonGroupWord)
