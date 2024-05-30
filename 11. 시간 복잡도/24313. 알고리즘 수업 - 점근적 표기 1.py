05287127ebdbc547303b952b31ec6676ca993a29
7ee5u61n <le2471@gmail.com>
2024-05-30 21:36:43 +0900

    점근적 표기 1 해결

--- 11. 시간 복잡도/24313. 알고리즘 수업 - 점근적 표기 1.py
+++ 11. 시간 복잡도/24313. 알고리즘 수업 - 점근적 표기 1.py
@@ -1,1 +1,13 @@
-a1, a0 = map(int)
+a1, a0 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
+answer = 0
+for i in range(n0, 101):
+    if a1 * i + a0 > c * i:
+        answer = 0
+        break
+    else:
+        answer = 1
+
+print(answer)
