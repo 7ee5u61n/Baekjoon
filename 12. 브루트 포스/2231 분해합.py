0bc68c6add17741a7d5ba4d3ef22745a7fdf503a
7ee5u61n <le2471@gmail.com>
2024-06-01 23:18:26 +0900

    분해합 해결

--- 12. 브루트 포스/2231 분해합.py
+++ 12. 브루트 포스/2231 분해합.py
@@ -1,14 +1,14 @@
 N = int(input())
 
 generator = []
-
-i = 0
-for i in range(N):
-    i += 1
-    j = 0
+for i in range(1, N+1):
+    j = 1
     wholeSum = 0
     for j in range(len(str(i))):
         j += 1
-        wholeSum += (i % ((10**j)//(10**(j-1)))) # 각 자리수의 합
-    generator.append(wholeSum + i)
-print(generator)
+        wholeSum += (i % (10**j)//(10**(j-1))) # 각 자리수의 합
+    if wholeSum + i == N: # 가장 작은 생성자
+        print(i)
+        break
+    if i == N:
+        print(0)
