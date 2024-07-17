830b01cdb7b7e2ab945e42822b452e0315a2d9f6
7ee5u61n <le2471@gmail.com>
2024-07-17 22:14:36 +0900

    별 찍기 - 3

--- test.py
+++
@@ -1,8 +0,0 @@
-while True:
-    name, age, weight = list(input().split())
-    if name =='#' and age == '0' and weight == '0':
-        break
-    if int(age) > 17 or int(weight) >= 80:
-        print(name, 'Senior')
-    else:
-        print(name, 'Junior')
