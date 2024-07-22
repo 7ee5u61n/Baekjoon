12c85cc6aeb1570fa7f1fb930e6743c28c5c4163
7ee5u61n <le2471@gmail.com>
2024-07-22 21:24:19 +0900

    부호 해결

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-N
+import sys
+
+for _ in range(3):
+    S = 0
+    N = int(sys.stdin.readline())
+    for i in range(N):
+        S += int(sys.stdin.readline())
+    if S == 0:
+        sys.stdout.write('0'+ '\n')
+    elif S >= 0:
+        sys.stdout.write('+' + '\n')
+    else:
+        sys.stdout.write('-' + '\n')
