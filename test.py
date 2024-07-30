469211668ccc8b4ea3ea36b57f061eace0f4b742
7ee5u61n <le2471@gmail.com>
2024-07-30 15:20:24 +0900

    수 찾기 해결

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-ABC = list(map(int, input().split()))
+import sys
+N = int(sys.stdin.readline())
+A = set(map(int, sys.stdin.readline().split()))
 
-ABC.pop(ABC.index(max(ABC)))
-print(max(ABC))
+M = int(sys.stdin.readline())
+MA = list(map(int, sys.stdin.readline().split()))
+for i in range(M):
+    if MA[i] in A:
+        print(1)
+    else:
+        print(0)
