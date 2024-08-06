d362c8a40488a7cc05b637446150b971bb77494f
7ee5u61n <le2471@gmail.com>
2024-08-06 21:55:32 +0900

    N과 M (1) 재도전

--- 22. 백트래킹/15649 N과 M (1).py
+++ 22. 백트래킹/15649 N과 M (1).py
@@ -1,22 +1,5 @@
-import sys
+from itertools import permutations
 
-# N = N까지의 자연수, M = 뽑는 갯수, A = 수열, C = 이미 고른 것
-def pick(N, M, A, C):
-    if len(A) == M:
-        print(*A)
-        return
-    for i in range(1, N+1):
-        if C[i]:
-            continue
-        C[i] = True
-        A.append(i)
-        pick(N, M, A, C)
-        A.pop()
-        C[i] = False
-
-N, M = map(int, sys.stdin.readline().split())
-
-arr = []
-check = [False] * (N+1)
-
-pick(N, M, arr, check)
+N, M = map(int, input().split())
+N = list(map(str, range(1, N+1)))
+print('\n'.join(list(map(' '.join, permutations(N, M)))))
