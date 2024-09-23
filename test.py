5a95c16693142945aeb30533bdbee02f9e686864
7ee5u61n <le2471@gmail.com>
2024-09-23 15:34:55 +0900

    캠프가는 영식 해결

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 N, T = map(int, input().split())
 
-next_bus = 0
-ride_bus = -1
+result = []
 for _ in range(N):
     S, I, C = map(int, input().split())
     
-    next_bus = S
-    for i in range(C):
-        next_bus += I * i
-        if next_bus >= T:
-            ride_bus = next_bus - T
-            break
+    bus_time = [S+I*i for i in range(C)]
+    if bus_time[-1] < T:
+        continue
 
-print(ride_bus)
+    start = 0
+    end = C - 1
+    target = 0
+    while start <= end:
+        mid = (start + end) // 2
+        if bus_time[mid] >= T:
+            target = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    result.append(bus_time[target] - T)
+
+if result:
+    print(min(result))
+else:
+    print(-1)
