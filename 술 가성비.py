08bb0ee5be5e03d6db10f83a4165ac95c31c6e4f
7ee5u61n <le2471@gmail.com>
2024-09-28 12:17:51 +0900

    술 가성비

---
+++ 술 가성비.py
@@ -0,0 +1,5 @@
+print('가격: 원, 용량: ml, 도수: %. 순으로 입력')
+price = capacity = volume = 1
+while price != 0 and capacity != 0 and volume != 0:
+    price, capacity, volume = map(int, input().split())
+    print("%3f" %((capacity*volume/price)))
