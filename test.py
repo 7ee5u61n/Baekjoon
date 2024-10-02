52d9019ae6ed58cc10b462d3e43854cf9fd0bc52
7ee5u61n <le2471@gmail.com>
2024-10-02 18:55:52 +0900

    정보보호학부 동아리 해결

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
-# 플레이어 수
-N = int(input())
-# 투표한 번호
-X = list(map(int, input().split()))
-# 받은 투표 수
-player = dict()
-for i in range(N):
-    player[i+1] = 0
+initial = {'M': 'MatKor',
+           'W': 'WiCys',
+           'C': 'CyKor',
+           'A': 'AlKor',
+           '$': '$clear'}
 
-for i in range(N):
-    vote = X[i]
-    if vote != 0:
-        player[vote] += 1
-
-# 가장 많이 표를 받은 사람
-imposter = [k for k,v in player.items() if max(player.values()) == v]
-
-if len(imposter) == 1:
-    print(*imposter)
-else:
-    print('skipped')
+print(initial[input()])
