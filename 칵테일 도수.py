4adddcb7958854770cd723e09c3c984e1fd1257c
7ee5u61n <le2471@gmail.com>
2024-10-18 18:14:21 +0900

    칵테일 도수

---
+++ 칵테일 도수.py
@@ -0,0 +1,16 @@
+print('재료 개수')
+n = int(input())
+
+numerator = 0
+denominator = 0
+for i in range(n):
+    print('용량, 도수')
+    a, b = map(int, input().split())
+    numerator += (a*b)
+    denominator += a
+
+volume = round(numerator/denominator, 1)
+alcohol = round(volume*0.01*denominator*0.8)
+
+print('도수: %, 알코올량: g')
+print(volume, alcohol)
