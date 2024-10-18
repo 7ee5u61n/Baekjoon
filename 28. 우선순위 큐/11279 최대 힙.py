75cd5b326378717c09a673daf4dcd5df2d253873
7ee5u61n <le2471@gmail.com>
2024-10-18 16:48:29 +0900

    최대 힙 해결

---
+++ 28. 우선순위 큐/11279 최대 힙.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+max_heap = []
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if max_heap:     
+            print(heapq.heappop(max_heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(max_heap, (-x, x))
