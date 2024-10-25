764dee030d6cee4394e727a9020e31f5906a93d6
7ee5u61n <le2471@gmail.com>
2024-10-25 11:54:44 +0900

    뱀 2회 해결

--- 삼성 SW 역량 테스트 기출 문제/3190 뱀.py
+++ 삼성 SW 역량 테스트 기출 문제/3190 뱀.py
@@ -4,69 +4,60 @@ from collections import deque
 n = int(input())
 # 사과의 개수
 k = int(input())
-
-# 사과의 위치
 apples = []
 for i in range(k):
     row, column = map(int, input().split())
     apples.append([column, row])
 
+# 방향 전환 횟수
+l = int(input())
+turn = []
+for i in range(l):
+    x, c = input().split()
+    turn.append([x, c])
 head = [1, 1]
 tail = deque()
 
-# 움직이는 방향 (동, 남, 서, 북)
-di = 0
+# 동, 남, 서, 북
+d = 0
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-
-# 방향 변환 횟수
-l = int(input())
-move = deque()
-for i in range(l):
-    move.append(input().split())
-
 time = 0
 while True:
-    # 시간
     time += 1
+    nx = head[0]+dx[d]
+    ny = head[1]+dy[d]
+    # 다음 이동할 칸
+    next_head = [nx, ny]
 
-    # 다음 머리 위치
-    next_head = [head[0]+dx[di], head[1]+dy[di]]
     # print(apples)
-    # print(f'방향: {di}')
-    # print(f'next_head: {next_head, time}')
-    # print(f'head:{head, time}')
-    # print(f'tail:{tail, time}')
-    tail.appendleft(head)
-    if next_head not in tail:
-        # 맵 안
-        if 1 <= next_head[0] <= n and 1 <= next_head[1] <= n:
-            # 사과 먹음
-            if next_head in apples:
-                # print('eat', time)
-                apples.remove(next_head)
-                head = next_head
-            else:
-                if tail:
-                    tail.pop()
-                head = next_head
-        # 맵 벗어남
-        else:
-            # print('맵 벗어남')
-            break
-    # 꼬리에 닿음
-    else:
-        # print('꼬리 충돌')
+    # print(next_head, time)
+    # print(head, time)
+    # print(tail, time)
+    
+    # 꼬리에 충돌하는 경우
+    if next_head in tail:
         break
-        
+    if 1<=next_head[0]<=n and 1<=next_head[1]<=n:
+        # 사과를 먹은 경우
+        if next_head in apples:
+            apples.remove(next_head) # 사과가 맵에서 사라짐
+            tail.appendleft(head)
+            head = next_head
+        else:
+            tail.appendleft(head)
+            tail.pop()
+            head = next_head
+    # 벽을 만남
+    else:
+        break
     # 방향 전환 시간
-    if move:
-        if time == int(move[0][0]):
-            # 왼쪽
-            if move[0][1] == 'L':
-                di = (di-1)%4
+    if turn:
+        if time == int(turn[0][0]):
+            if turn[0][1] == 'L':
+                d = (d-1)%4 # 왼쪽
             else:
-                di = (di+1)%4
-            move.popleft()
+                d = (d+1)%4 # 오른쪽
+            turn.pop(0)
 
 print(time)
