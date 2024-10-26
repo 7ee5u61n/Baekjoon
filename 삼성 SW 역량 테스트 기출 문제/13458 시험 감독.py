7199f76deb31c6b73b9deaec3a80b312b779644e
7ee5u61n <le2471@gmail.com>
2024-10-26 16:11:12 +0900

    시험 감독 해결

--- 삼성 SW 역량 테스트 기출 문제/13458 시험 감독.py
+++ 삼성 SW 역량 테스트 기출 문제/13458 시험 감독.py
@@ -8,11 +8,9 @@ b, c = map(int, sys.stdin.readline().split())
 # 필요한 감독 수
 count = 0
 for stu in a:
+    count += 1
     # 부감독 필요
     if stu - b > 0:
-        count += 1
         count += math.ceil((stu-b)/c)
-    else:
-        count += 1
 
 print(count)
