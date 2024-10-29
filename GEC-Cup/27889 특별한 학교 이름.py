c476a58e55edb9c63892b241670dbf17af8be334
7ee5u61n <le2471@gmail.com>
2024-10-29 19:30:12 +0900

    삭제

--- GEC-Cup/27889 특별한 학교 이름.py
+++
@@ -1,9 +0,0 @@
-a = input()
-if a == 'NLCS':
-    print('North London Collegiate School')
-elif a == 'BHA':
-    print('Branksome Hall Asia')
-elif a == ('KIS'):
-    print('Korea International School')
-elif a == ('SJA'):
-    print('St. Johnsbury Academy')
