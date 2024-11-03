f67ec86ad3cfafcb833fa2936f01b39d1445e47c
7ee5u61n <le2471@gmail.com>
2024-11-03 16:01:00 +0900

    N과 M (1) 재풀이

--- 22. 백트래킹/15649 N과 M (1).py
+++ 22. 백트래킹/15649 N과 M (1).py
@@ -1,5 +1,19 @@
-from itertools import permutations
+def dfs(s):
+    global result
+    if len(s) == m:
+        result.append(s)
+        return
+    for i in range(1, n+1):
+        if visited[i] == False:
+            visited[i] = True
+            dfs(s+[i])
+            visited[i] = False
+        
+n, m = map(int, input().split())
 
-N, M = map(int, input().split())
-N = list(map(str, range(1, N+1)))
-print('\n'.join(list(map(' '.join, permutations(N, M)))))
+visited = [False]*(n+1)
+result = []
+dfs([])
+
+for i in result:
+    print(*i)
