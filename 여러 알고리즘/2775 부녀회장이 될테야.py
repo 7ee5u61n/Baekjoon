7a7ab35ea0c201172da0ae5172511947cffdb5f3
7ee5u61n <le2471@gmail.com>
2024-11-05 21:28:55 +0900

    부녀회장이 될테야 해결

--- 여러 알고리즘/2775 부녀회장이 될테야.py
+++ 여러 알고리즘/2775 부녀회장이 될테야.py
@@ -1,11 +1,20 @@
 T = int(input())
-for test_case in range(1, T+1):
+for _ in range(T):
     k = int(input())
     n = int(input())
-    floor = [i for i in range(1, n+1)]
-    
-    for i in range(k):
-        for j in range(1, n):
-            floor[j] += floor[j-1]
 
-    print(floor[-1])
+    # 아래 층
+    base = [i for i in range(n+1)]
+    # 현재 층
+    dp = [1]*(n+1)
+    for i in range(1, k+1):
+        for j in range(1, n+1):
+            # 1호는 항상 1명
+            if j == 1:
+                dp[j] = 1
+            # n-1호 + 아래집
+            else:
+                dp[j] = dp[j-1] + base[j]
+        base = dp
+
+    print(dp[n])
