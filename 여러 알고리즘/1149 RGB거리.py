c3f35e2591a1255e6fa18f6bff0e14fdd1ec4ea0
7ee5u61n <le2471@gmail.com>
2024-11-05 21:40:46 +0900

    RGB거리 해결

--- 여러 알고리즘/1149 RGB거리.py
+++ 여러 알고리즘/1149 RGB거리.py
@@ -2,7 +2,8 @@ n = int(input())
 paint = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[0]*3 for _ in range(n)]
-dp[0] = paint[0]
+for i in range(3):
+    dp[0][i] = paint[0][i]
 
 for i in range(1, n):
     dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + paint[i][0]
