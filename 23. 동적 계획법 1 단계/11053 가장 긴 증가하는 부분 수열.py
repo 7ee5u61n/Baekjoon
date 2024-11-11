90a9cbb08313856137ec0ecf4dd06349d8f24b4d
7ee5u61n <le2471@gmail.com>
2024-11-11 23:05:14 +0900

    가장 긴 증가하는 부분 수열 재풀이

--- 23. 동적 계획법 1 단계/11053 가장 긴 증가하는 부분 수열.py
+++ 23. 동적 계획법 1 단계/11053 가장 긴 증가하는 부분 수열.py
@@ -1,11 +1,10 @@
 n = int(input())
-A = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
 dp = [1]*n
-
 for i in range(n):
     for j in range(i):
-        if A[i] > A[j]:
+        if a[i] > a[j]:
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
