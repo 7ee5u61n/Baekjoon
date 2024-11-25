1af760df763ab6b4fea8396fc0631a44a523e733
7ee5u61n <le2471@gmail.com>
2024-11-25 20:21:14 +0900

    필터 해결

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-N, K = map(int, input().split())
+r, c = map(int, input().split())
+pixel = [list(map(int, input().split())) for _ in range(r)]
+t = int(input())
 
+filter = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
 count = 0
-for i in range(N+1):
-    for j in range(60):
-        for k in range(60):
-            time = str(i).zfill(2) + str(j).zfill(2) + str(k).zfill(2)
-            if str(K) in time:
-                count += 1
+for i in range(r-2):
+    for j in range(c-2):
+        value = []
+        for dx, dy in filter:
+            value.append(pixel[i+dx][j+dy])
+        value.sort()
+        if value[4] >= t:
+            count += 1
 
 print(count)
