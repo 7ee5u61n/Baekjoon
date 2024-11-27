5d135b6cc6b2fc6fed8e3517327620e761b3be5c
7ee5u61n <le2471@gmail.com>
2024-11-27 20:10:04 +0900

    Card Game Contest

--- test.py
+++ test.py
@@ -1,16 +1,8 @@
-r, c = map(int, input().split())
-pixel = [list(map(int, input().split())) for _ in range(r)]
-t = int(input())
-
-filter = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
-count = 0
-for i in range(r-2):
-    for j in range(c-2):
-        value = []
-        for dx, dy in filter:
-            value.append(pixel[i+dx][j+dy])
-        value.sort()
-        if value[4] >= t:
-            count += 1
-
-print(count)
+n, m = map(int, input().split())
+result = 1
+for _ in range(n):
+    a = int(input())
+    if a != 0:
+        result *= a 
+        
+print(result%m)
