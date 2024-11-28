0cad15f68fdb7ea1bd1c1f68e545a9a4af7f9fd2
7ee5u61n <le2471@gmail.com>
2024-11-28 20:11:45 +0900

    울타리 공사 해결

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-n, m = map(int, input().split())
-result = 1
-for _ in range(n):
-    a = int(input())
-    if a != 0:
-        result *= a 
-        
-print(result%m)
+def calcArea(a, b, c, d):
+    return ((c-a)+(d-b))*2
+
+n = int(input())
+a, b, c, d = map(int, input().split())
+print(calcArea(a, b, c, d))
+
+for _ in range(n-1):
+    newA, newB, newC, newD = map(int, input().split())
+    a = min(a, newA)
+    b = min(b, newB)
+    c = max(c, newC)
+    d = max(d, newD)
+    print(calcArea(a,b,c,d))
