02b277bafe7e38fce0610e62025a535487aee858
7ee5u61n <le2471@gmail.com>
2024-12-01 17:24:39 +0900

    이동하기 3 해결

--- test.py
+++ test.py
@@ -1,22 +1,5 @@
-a, b, c, m = map(int, input().split())
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-figure = 0
-work = 0
-for i in range(24):
-    # 번아웃 오면 관둬
-    if figure > m:
-        work = 0
-        break
-    else:
-        # 일 할 수 있으면 해
-        if figure + a <= m:
-            figure += a
-            work += b
-        # 피곤하면 쉬어
-        else:
-            if figure - c >= 0:
-                figure -= c
-            else:
-                figure = 0
-
-print(work)
+print(sum(a)+sum(b))
