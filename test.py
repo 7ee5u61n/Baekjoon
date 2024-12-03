f553689f7f0c808bfbcc4c9cdbe41a0d1ebe05f1
7ee5u61n <le2471@gmail.com>
2024-12-03 10:14:56 +0900

    어두운 굴다리 해결

--- test.py
+++ test.py
@@ -1,28 +1,13 @@
+import math
+
 n = int(input())
+m = int(input())
+x = list(map(int, input().split()))
 
-arr = {'ADD':'0000', 'SUB':'0001', 'MOV':'0010', 'AND':'0011','OR':'0100',
-                   'NOT':'0101','MULT':'0110', 'LSFTL':'0111', 'LSFTR':'1000',
-                   'ASFTR':'1001','RL':'1010','RR':'1011'}
+result = x[0]
+for i in range(1, m):
+    value = math.ceil((x[i]-x[i-1])/2)
+    result = max(result, value)
+result = max(result, n-x[-1])
 
-for _ in range(n):
-    opcode, rD, rA, rB = input().split()
-    machine_language = ''
-    
-    if opcode[-1]=='C':
-        machine_language += arr.get(opcode[:-1])
-        machine_language += '1'
-    else:
-        machine_language += arr.get(opcode)
-        machine_language += '0'
-    machine_language += '0'
-    
-    machine_language += str(bin(int(rD)))[2:].zfill(3)
-    machine_language += str(bin(int(rA)))[2:].zfill(3)
-    
-    if machine_language[4] == '0':
-        machine_language += str(bin(int(rB)))[2:].zfill(3)
-        machine_language += '0'
-    else:
-        machine_language += str(bin(int(rB)))[2:].zfill(4)
-        
-    print(machine_language)
+print(result)
