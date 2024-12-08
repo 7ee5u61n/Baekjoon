972adce2fc953242f714a669f8e1d3beefd5f0ed
7ee5u61n <le2471@gmail.com>
2024-12-08 21:14:14 +0900

    오르막길 해결

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-def intergration(x, a, b, c, d, e):
-    return (((a*x**3)//3) + ((b*x**2)//2) + (c*x)) - (((d*x**2)//2) + (e*x))
+n = int(input())
+height = list(map(int, input().split()))
 
-x1, x2 = map(int, input().split())
-a, b, c, d, e = map(int, input().split())
+max_upper = 0
+upper = 0
+for i in range(n-1):
+    if height[i] < height[i+1]:
+        upper += height[i+1]-height[i]
+    else:
+        upper = 0
+    max_upper = max(upper, max_upper)
 
-power = intergration(x2, a, b, c, d, e) - intergration(x1, a, b, c, d, e)
-print(power)
+print(max_upper)
