6b688bad97418c7cdbc81efbba6c94e72b1229da
7ee5u61n <le2471@gmail.com>
2024-12-13 18:00:22 +0900

    싸이클 해결

--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-order = str(input())
+n, p = map(int, input().split())
 
-robot = 0
-box = 0
-goal = 0
+arr = []
+value = n
+while True:
+    value = (value*n)%p
+    if value in arr:
+        break
+    else:
+        arr.append(value)
 
-for i in range(len(order)):
-    if order[i] == '@':
-        robot = i
-    elif order[i] == '#':
-        box = i
-    elif order[i] == '!':
-        goal = i
-
-if robot < box < goal:
-    print(goal - robot - 1)
-elif robot > box > goal:
-    print(robot - goal - 1)
-else:
-    print(-1)
+print(len(arr) - arr.index(value))
