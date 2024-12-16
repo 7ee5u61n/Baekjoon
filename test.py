334ee2b0808ad515d473df28140d438fbb6d393a
7ee5u61n <le2471@gmail.com>
2024-12-16 20:49:11 +0900

    게시판 해결

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-from collections import deque
-import queue
+T = int(input())
+for _ in range(T):
+    x1, y1, x2, y2, x3, y3, x4, y4 = map(int, input().split())
+    
+    # 겹치는 부분 좌표
+    dx = []
+    dy = []
+    for i in range(x3, x4+1):
+        if x1 <= i <= x2:
+            dx.append(i)
+    for i in range(y3, y4+1):
+        if y1 <= i <= y2:
+            dy.append(i)
 
-s = str(input())
-n = int(input())
+    # 포스터 넓이
+    poster1 = (x2-x1)*(y2-y1)
+    # 겹치는 부분 넓이
+    poster2 = 0
+    if dx and dy:
+        poster2 = (dx[-1]-dx[0])*(dy[-1]-dy[0])
 
-count = 0
-for _ in range(n):
-    ring = (str(input())*2)
-    for i in range(len(ring)):
-        if ring[i:i+len(s)] == s:
-            count += 1
-            break
-        
-print(count)
+    result = poster1 - poster2
+    print(result)
