98fa74e412c96b930065d5937282ea79bdbe739c
7ee5u61n <le2471@gmail.com>
2024-12-17 20:38:07 +0900

    대표 자연수 해결

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-T = int(input())
-for _ in range(T):
-    x1, y1, x2, y2, x3, y3, x4, y4 = map(int, input().split())
-    
-    # 겹치는 부분 좌표
-    dx = []
-    dy = []
-    for i in range(x3, x4+1):
-        if x1 <= i <= x2:
-            dx.append(i)
-    for i in range(y3, y4+1):
-        if y1 <= i <= y2:
-            dy.append(i)
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
 
-    # 포스터 넓이
-    poster1 = (x2-x1)*(y2-y1)
-    # 겹치는 부분 넓이
-    poster2 = 0
-    if dx and dy:
-        poster2 = (dx[-1]-dx[0])*(dy[-1]-dy[0])
+prime = 10000
+least = 20000*10000
+for i in range(n):
+    sm = 0
+    for j in range(n):
+        sm += abs(arr[i]-arr[j])
+    if sm < least:
+        least = sm
+        prime = arr[i]
 
-    result = poster1 - poster2
-    print(result)
+print(prime)
