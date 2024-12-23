71adb3444f9e714259811e8143fd4dd1f18d6c73
7ee5u61n <le2471@gmail.com>
2024-12-23 19:21:25 +0900

    주사위 게임 해결

--- test.py
+++ test.py
@@ -1,35 +1,13 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
+n = int(input())
+changyeong = 100
+sangdeok = 100
 
-for _ in range(m):
-    order = list(map(int, input().split()))
-    # k보다 크거나 같은 원소
-    if order[0] == 1:
-        count = 0
-        for i in range(n-1, -1, -1):
-            if a[i] >= order[1]:
-                count += 1
-            else:
-                break
-        print(count)
-    
-    # k보다 큰 원소
-    elif order[0] == 2:
-        count = 0
-        for i in range(n-1, -1, -1):
-            if a[i] > order[1]:
-                count += 1
-            else:
-                break
-        print(count)
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a < b:
+        changyeong -= b
+    elif a > b:
+        sangdeok -= a
 
-    # i보다 크거나 같고 j보다 작거나 같은 원소
-    else:
-        count = 0
-        for i in range(n):
-            if order[1] <= a[i] <= order[2]:
-                count += 1
-            elif a[i] > order[2]:
-                break
-        print(count)
+print(changyeong)
+print(sangdeok)
