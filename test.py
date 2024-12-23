5e6c96454c93d7e6028346827f100cdfb136b3be
7ee5u61n <le2471@gmail.com>
2024-12-23 18:23:35 +0900

    상근이의 친구들 해결

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-n = int(input())
-student = []
-for _ in range(n):
-    student.append(''.join(reversed(str(input()))))
-
-word = len(student[0])
-for i in range(1, word+1):
-    number = []
-    for j in range(n):
-        number.append((student[j])[:i])
-    if len(number) == len(list(set(number))):
+while True:
+    m, f = map(int, input().split())
+    if m == 0 and f == 0:
         break
-
-print(i)
+    else:
+        print(m+f)
