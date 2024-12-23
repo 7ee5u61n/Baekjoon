958a0d506ca894e6d815077cd007035e803937ea
7ee5u61n <le2471@gmail.com>
2024-12-23 19:18:13 +0900

    배열 전체 탐색하기 부분 해결

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
-while True:
-    m, f = map(int, input().split())
-    if m == 0 and f == 0:
-        break
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+
+for _ in range(m):
+    order = list(map(int, input().split()))
+    # k보다 크거나 같은 원소
+    if order[0] == 1:
+        count = 0
+        for i in range(n-1, -1, -1):
+            if a[i] >= order[1]:
+                count += 1
+            else:
+                break
+        print(count)
+    
+    # k보다 큰 원소
+    elif order[0] == 2:
+        count = 0
+        for i in range(n-1, -1, -1):
+            if a[i] > order[1]:
+                count += 1
+            else:
+                break
+        print(count)
+
+    # i보다 크거나 같고 j보다 작거나 같은 원소
     else:
-        print(m+f)
+        count = 0
+        for i in range(n):
+            if order[1] <= a[i] <= order[2]:
+                count += 1
+            elif a[i] > order[2]:
+                break
+        print(count)
