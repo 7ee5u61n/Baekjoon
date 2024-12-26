ebfadc3568f27f725fcfc30d0cfc72bd6b1e2d20
7ee5u61n <le2471@gmail.com>
2024-12-26 21:06:07 +0900

    공백 없는 A+B 해결

--- test.py
+++ test.py
@@ -1,22 +1,17 @@
-l, r, a = map(int, input().split())
+ab = str(input())
 
-# 왼발, 오른발 같을 때 까지
-while l != r:
-    if l > r:
-        # 양발 있으면
-        if a:
-            a -= 1
-            r += 1
-        # 없으면
-        else:
-            l -= 1
-    elif l < r:
-        if a:
-            a -= 1
-            l += 1
-        else:
-            r -= 1
-if a % 2 == 1:
-    a -= 1
-
-print(l+r+a)
+if len(ab) == 2:
+    a = int(ab[0])
+    b = int(ab[1])
+elif len(ab) == 4:
+    a = int(ab[0:2])
+    b = int(ab[2:4])
+else:
+    if ab[-1] == '0':
+        a = int(ab[0])
+        b = int(ab[1:3])
+    else:
+        a = int(ab[0:2])
+        b = int(ab[2])
+        
+print(a+b)
