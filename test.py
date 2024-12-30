3e0e6c66b11aaaa5fedc254ee3d4ea5b5859c5d2
7ee5u61n <le2471@gmail.com>
2024-12-30 19:25:55 +0900

    캥거루 세마리 해결

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-n = int(input())
-text = list(str(input()) for _ in range(n))
+a, b, c = map(int, input().split())
 
-number = []
-for i in range(n):
-    num = ''
-    for j in range(len(text[i])):
-        if text[i][j].isnumeric():
-            num += text[i][j]
-        else:
-            if num:
-                number.append(int(num))
-            num = ''
-    if num:
-        number.append(int(num))
-
-number.sort()
-for i in number:
-    print(i)
+print(max(b-a-1, c-b-1))
