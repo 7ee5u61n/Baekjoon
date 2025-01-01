17e1852859d81266fc614bbb42fb3987b679e352
7ee5u61n <le2471@gmail.com>
2025-01-01 20:56:18 +0900

    코드마스터 2023 해결

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-n = int(input())
-arr = list(map(str, input().split('*')))
+guho = str(input())
 
-left = arr[0]
-right = arr[1]
-
-for _ in range(n):
-    name = str(input())
-
-    if len(name) < len(left)+len(right):
-        print('NE')
-    else:
-        if name[:len(left)] == left and name[-len(right):] == right:
-            print('DA')
-        else:
-            print('NE')
+if guho == 'SONGDO':
+    print('HIGHSCHOOL')
+elif guho == 'CODE':
+    print('MASTER')
+elif guho == '2023':
+    print('0611')
+elif guho == 'ALGORITHM':
+    print('CONTEST')
