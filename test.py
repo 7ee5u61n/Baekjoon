6bf33bd437132fed87decdaa6781abd4ee9d13fe
7ee5u61n <le2471@gmail.com>
2025-01-02 21:41:52 +0900

    운동회 해결

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-guho = str(input())
+import math
 
-if guho == 'SONGDO':
-    print('HIGHSCHOOL')
-elif guho == 'CODE':
-    print('MASTER')
-elif guho == '2023':
-    print('0611')
-elif guho == 'ALGORITHM':
-    print('CONTEST')
+n, m, a, k = map(int, input().split())
+
+low = min(n, a-k+1)
+high = math.ceil((a-k)/m)+1
+
+print(low, high)
