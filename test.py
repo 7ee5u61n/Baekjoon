574286f8e9b174d864cdd4cb59c1c90be5b71a42
7ee5u61n <le2471@gmail.com>
2025-01-03 19:30:15 +0900

    암기왕 해결

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-import math
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    note1 = set(map(int, input().split()))
+    m = int(input())
+    note2 = list(map(int, input().split()))
 
-n, m, a, k = map(int, input().split())
-
-low = min(n, a-k+1)
-high = math.ceil((a-k)/m)+1
-
-print(low, high)
+    for i in note2:
+        if i in note1:
+            print(1)
+        else:
+            print(0)
