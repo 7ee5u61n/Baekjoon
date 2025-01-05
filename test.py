0e2954c7347d16890410a81adcb4de2a7e520678
7ee5u61n <le2471@gmail.com>
2025-01-05 19:36:18 +0900

    이변마작 1 해결

--- test.py
+++ test.py
@@ -1,70 +1,16 @@
-T = int(input())
-for test_case in range(1, T+1):
-    board = list(input() for _ in range(3))
-    ox = input()
-    
-    arr = []
-    for i in range(3):
-        for j in range(3):
-            arr.append(board[i][j])
-    seted = False
-    if not seted:
-        # 가로
-        for i in range(3):
-            count = 0
-            for j in range(3):
-                if arr[3*i+j] == ox:
-                    count += 1
-            
-            if count == 2:
-                seted = True
-                for j in range(3):
-                    if arr[3*i+j] != ox:
-                        arr[3*i+j] = ox
-                        break
-    if not seted:
-        # 세로
-        for i in range(3):
-            count = 0
-            for j in range(3):
-                if arr[i+3*j] == ox:
-                    count += 1
-            if count == 2:
-                seted = True
-                for j in range(3):
-                    if arr[i+3*j] != ox:
-                        arr[i+3*j] = ox
-                        break
-    
-    if not seted:
-        # 대각선 아래
-        count = 0
-        for i in range(3):
-            if arr[4*i] == ox:
-                count += 1
-        if count == 2:
-            seted = True
-            for i in range(3):
-                if arr[4*i] != ox:
-                    arr[4*i] = ox
-                    break
+n = int(input())
+mahjong = list(map(str, input().split()))
 
-    if not seted:
-        # 대각선 위
-        count = 0
-        for i in range(1, 4):
-            if arr[2*i] == ox:
-                count += 1
-        if count == 2:
-            seted = True
-            for i in range(1, 4):
-                if arr[2*i] != ox:
-                    arr[2*i] = ox
-                    break
-    
-    # 출력
-    print(f'Case {test_case}:')
-    for i in range(9):
-        print(arr[i], end='')
-        if i%3 == 2:
-            print('')
+card = {'1m':0, '2m':0, '3m':0, '4m':0, '5m':0, '6m':0, '7m':0, '8m':0, '9m':0, 
+    '1p':0, '2p':0, '3p':0, '4p':0, '5p':0, '6p':0, '7p':0, '8p':0, '9p':0,
+    '1s':0, '2s':0, '3s':0, '4s':0, '5s':0, '6s':0, '7s':0, '8s':0, '9s':0,
+    '1z':0, '2z':0, '3z':0, '4z':0, '5z':0, '6z':0, '7z':0}
+
+result = 0
+for i in range(n):
+    card[mahjong[i]] += 1
+    if card[mahjong[i]] == 5:
+        result = i+1
+        break
+
+print(result)
