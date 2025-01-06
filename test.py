6acb3f9c14dc33d38e2f5ca348e93dd3513fa118
7ee5u61n <le2471@gmail.com>
2025-01-06 20:23:43 +0900

    상금 헌터 해결

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-n = int(input())
-mahjong = list(map(str, input().split()))
+T = int(input())
+for _ in range(T):
+    c1, c2 = map(int, input().split())
+    price = 0
+    
+    if 0 < c1 <= 1:
+        price += 5000000
+    elif 1 < c1 <= 3:
+        price += 3000000
+    elif 3 < c1 <= 6:
+        price += 2000000
+    elif 6 < c1 <= 10:
+        price += 500000
+    elif 10 < c1 <= 15:
+        price += 300000
+    elif 15 < c1 <= 21:
+        price += 100000
+    
+    if 0 < c2 <= 1:
+        price += 5120000
+    elif 1 < c2 <= 3:
+        price += 2560000
+    elif 3 < c2 <= 7:
+        price += 1280000
+    elif 7 < c2 <= 15:
+        price += 640000
+    elif 11 < c2 <= 31:
+        price += 320000
 
-card = {'1m':0, '2m':0, '3m':0, '4m':0, '5m':0, '6m':0, '7m':0, '8m':0, '9m':0, 
-    '1p':0, '2p':0, '3p':0, '4p':0, '5p':0, '6p':0, '7p':0, '8p':0, '9p':0,
-    '1s':0, '2s':0, '3s':0, '4s':0, '5s':0, '6s':0, '7s':0, '8s':0, '9s':0,
-    '1z':0, '2z':0, '3z':0, '4z':0, '5z':0, '6z':0, '7z':0}
-
-result = 0
-for i in range(n):
-    card[mahjong[i]] += 1
-    if card[mahjong[i]] == 5:
-        result = i+1
-        break
-
-print(result)
+    print(price)
