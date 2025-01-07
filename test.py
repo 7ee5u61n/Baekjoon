294d61230f03bae93d4cc64cad23ecbf437f6283
7ee5u61n <le2471@gmail.com>
2025-01-07 16:22:10 +0900

    눈덩이 굴리기 해결

--- test.py
+++ test.py
@@ -1,30 +1,20 @@
-T = int(input())
-for _ in range(T):
-    c1, c2 = map(int, input().split())
-    price = 0
-    
-    if 0 < c1 <= 1:
-        price += 5000000
-    elif 1 < c1 <= 3:
-        price += 3000000
-    elif 3 < c1 <= 6:
-        price += 2000000
-    elif 6 < c1 <= 10:
-        price += 500000
-    elif 10 < c1 <= 15:
-        price += 300000
-    elif 15 < c1 <= 21:
-        price += 100000
-    
-    if 0 < c2 <= 1:
-        price += 5120000
-    elif 1 < c2 <= 3:
-        price += 2560000
-    elif 3 < c2 <= 7:
-        price += 1280000
-    elif 7 < c2 <= 15:
-        price += 640000
-    elif 11 < c2 <= 31:
-        price += 320000
+def snow(size, index, count):
+    global result
 
-    print(price)
+    if count > m:
+        return
+    
+    if count <= m:
+        result = max(result, size)
+    
+    if index + 1 <= n:
+        snow(size+a[index+1], index+1, count+1)
+    if index + 2 <= n:
+        snow(size//2+a[index+2], index+2, count+1)
+
+n, m = map(int, input().split())
+a = [0] + list(map(int, input().split()))
+
+result = -1
+snow(1, 0, 0)
+print(result)
