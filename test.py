f602680d6c2d938e68b2de9ee32f3475d95cdca0
7ee5u61n <le2471@gmail.com>
2025-01-08 18:48:04 +0900

    민주주의 해결

--- test.py
+++ test.py
@@ -1,20 +1,17 @@
-def snow(size, index, count):
-    global result
-
-    if count > m:
-        return
-    
-    if count <= m:
-        result = max(result, size)
-    
-    if index + 1 <= n:
-        snow(size+a[index+1], index+1, count+1)
-    if index + 2 <= n:
-        snow(size//2+a[index+2], index+2, count+1)
-
 n, m = map(int, input().split())
-a = [0] + list(map(int, input().split()))
+candidate = list(input() for _ in range(n))
 
-result = -1
-snow(1, 0, 0)
+result = 0
+for i in candidate:
+    o = 0
+    x = 0
+    for j in i:
+        if j == 'O':
+            o += 1
+        else:
+            x += 1
+
+    if o > x:
+        result += 1
+
 print(result)
