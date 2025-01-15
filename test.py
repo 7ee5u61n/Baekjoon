1c85a30ddf562ce67ffac77382dcfbe74f8b203c
7ee5u61n <le2471@gmail.com>
2025-01-15 18:46:03 +0900

    안녕 클레오파트라 해결

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-T = int(input())
-for _ in range(T):
-    d, n, s, p = map(int, input().split())
-    if n * s > d + n * p:
-        print('parallelize')
-    elif n * s == d + n * p:
-        print('does not matter')
-    else:
-        print('do not parallelize')
+n, x = map(int, input().split())
+t = list(map(int, input().split()))
+
+index = 0
+while True:
+    if t[index] < x:
+        break
+    x += 1
+    index = (index+1)%len(t)
+
+print(index+1)
