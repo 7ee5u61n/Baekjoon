7dd73984435597b8c20cf0d1c6827cf81f2a5a08
7ee5u61n <le2471@gmail.com>
2025-01-16 21:29:50 +0900

    이름 궁합 해결

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-n, x = map(int, input().split())
-t = list(map(int, input().split()))
+a = str(input())
+b = str(input())
 
-index = 0
-while True:
-    if t[index] < x:
-        break
-    x += 1
-    index = (index+1)%len(t)
+stroke = [3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
 
-print(index+1)
+number = ''
+for i in range(len(a)):
+    number += str(stroke[ord(a[i])-65])
+    number += str(stroke[ord(b[i])-65])
+
+
+for i in range(len(a)*2-2):
+    now = ''
+    for j in range(len(number)-1):
+        if int(number[j]) + int(number[j+1]) >= 10:
+            now += str(int(number[j]) + int(number[j+1]) - 10)
+        else:
+            now += str(int(number[j]) + int(number[j+1]))
+    number = now
+
+print(number)
