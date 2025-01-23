68c3e5526ab9e037488c658fbd53d7095dd4f0db
7ee5u61n <le2471@gmail.com>
2025-01-23 23:13:54 +0900

    알고리즘 수업 - 선택 정렬 2 해결

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-dp = [1]*31
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
 
-for i in range(30):
-    dp[i+1] = dp[i]*2
+count = 0
+for i in range(n-1, 0, -1):
+    max_index = i
+    for j in range(i):
+        if arr[max_index] < arr[j]:
+            max_index = j
 
-n = int(input())
+    if arr[i] != arr[max_index]:
+        arr[i], arr[max_index]= arr[max_index], arr[i]
+        count += 1
+    
+    if count == k:
+        break
 
-if n in dp:
-    print(1)
+if count < k:
+    print(-1)
 else:
-    print(0)
+    print(*arr)
