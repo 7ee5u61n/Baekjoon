c03e58580eabfd7f7216274f292a4a55f16f626b
7ee5u61n <le2471@gmail.com>
2025-01-26 18:36:18 +0900

    노 땡스! 해결

--- test.py
+++ test.py
@@ -1,25 +1,20 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    pig = list(map(int, input().split()))
+n = int(input())
+x = list(map(int, input().split()))
+
+group = []
+temp = [x[0]]
+for i in range(1, n):
+    if x[i] - x[i-1] == 1:
+        temp.append(x[i])
+    else:
+        group.append(temp)
+        temp = [x[i]]
     
-    yesterday = [0]*6
-    for i in range(6):
-        yesterday[i] = pig[i]
+if temp:
+    group.append(temp)
 
-    day = 1
-    while True:
-        # 먹이 못 줘
-        if sum(pig) > n:
-            break
-        
-        for i in range(6):
-            # 전 날 , 왼쪽, 오른쪽, 맞은편
-            pig[i] = yesterday[i] + yesterday[(i-1)%6]+yesterday[(i+1)%6]+yesterday[(i+3)%6]
-        
-        for i in range(6):
-            yesterday[i] = pig[i]
-        
-        day += 1
+result = 0
+for part in group:
+    result += part[0]
 
-    print(day)
+print(result)
