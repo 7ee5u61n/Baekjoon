19ef622622ae898d474c8cee72cfa495ec683f8b
7ee5u61n <le2471@gmail.com>
2025-01-28 21:11:46 +0900

    접시 안의 원 해결

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-n, m = map(int, input().split())
-t, s = map(int, input().split())
-
-zip = n+(n-1)//8*s
-dok = t+m+(m-1)//8*(2*t+s)
-
-if zip > dok:
-    print('Dok')
-    print(dok)
-else:
-    print('Zip')
-    print(zip)
+t = int(input())
+result = round(t**2/4)
+print(result)
