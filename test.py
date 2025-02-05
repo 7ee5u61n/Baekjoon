fd3b843ca17ea2a961b680915eb747e8c3eeea79
7ee5u61n <le2471@gmail.com>
2025-02-05 19:05:05 +0900

    국방시계 해결

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
-n = int(input())
-winner = 1
-mx_score = 0
-for people in range(1, n+1):
-    card = list(map(int, input().split()))
-    score = 0
+n, m = map(int, input().split())
 
-    for i in range(5):
-        for j in range(i+1, 5):
-            for k in range(j+1, 5):
-                digit = (card[i]+card[j]+card[k])%10
-                if digit >= score:
-                    score = digit
-    
-    if score >= mx_score:
-        mx_score = score
-        winner = people
+total = int(1440*(m/n))
 
-print(winner)
+hour = str(total//60)
+minutes = str(total%60)
+
+if len(hour) == 1:
+    hour = '0'+hour
+if len(minutes) == 1:
+    minutes = '0'+minutes
+
+print(f'{hour}:{minutes}')
