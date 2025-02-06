682e9901d2235555e662471456b05df8859a89c2
7ee5u61n <le2471@gmail.com>
2025-02-06 20:26:28 +0900

    쉽게 푸는 문제 해결

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-n, m = map(int, input().split())
+a, b = map(int, input().split())
 
-total = int(1440*(m/n))
+arr = []
+for i in range(b+1):
+    arr += [i]*i
 
-hour = str(total//60)
-minutes = str(total%60)
-
-if len(hour) == 1:
-    hour = '0'+hour
-if len(minutes) == 1:
-    minutes = '0'+minutes
-
-print(f'{hour}:{minutes}')
+print(sum(arr[a-1:b]))
