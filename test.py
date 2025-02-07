4958e9b850f334af76491950425d267d62deafcb
7ee5u61n <le2471@gmail.com>
2025-02-07 20:00:10 +0900

    종이자르기 해결

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-a, b = map(int, input().split())
+x, y = map(int, input().split())
+row = [0, x]
+column = [0, y]
 
-arr = []
-for i in range(b+1):
-    arr += [i]*i
+n = int(input())
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a == 0:
+        column.append(b)
+    else:
+        row.append(b)
 
-print(sum(arr[a-1:b]))
+row.sort()
+column.sort()
+
+result = 0
+for i in range(1, len(row)):
+    for j in range(1, len(column)):
+        width = row[i] - row[i-1]
+        height = column[j] - column[j-1]
+        result = max(result, width*height)
+
+print(result)
