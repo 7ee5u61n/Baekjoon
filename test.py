abbc297c3f1409cb375c7aeeb7902b53f121b697
7ee5u61n <le2471@gmail.com>
2025-02-10 20:39:55 +0900

    수수수수퍼노바 해결

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-n, r = map(int, input().split())
-print(n+(r*2)-1)
+n, m = map(int, input().split())
+name = ''
+m -= 1
+if m // 26:
+    name = chr(m//26+96)+chr(m%26+97)
+else:
+    name = chr(m%26+65)
+
+print(f'SN {n}{name}')
