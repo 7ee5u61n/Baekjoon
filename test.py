58b0f717e0f533890ecea785f0e504f1e4aa4e81
7ee5u61n <le2471@gmail.com>
2025-02-13 18:18:23 +0900

    정수의 개수 해결

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-n, x = map(int, input().split())
+s = list(map(int, input().split(',')))
 
-bus = [list(map(int, input().split())) for _ in range(n)]
-bus.sort()
-
-result = -1
-for s, t in bus:
-    if s+t <= x:
-        result = s
-
-print(result)
+print(len(s))
