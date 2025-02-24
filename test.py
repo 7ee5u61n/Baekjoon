09fb566b1c0c3ee615338e5f1e6679ec38c72ef2
7ee5u61n <le2471@gmail.com>
2025-02-24 21:51:06 +0900

    불사조 해결

--- test.py
+++ test.py
@@ -1,23 +1,7 @@
-n = int(input())
-for test_case in range(1, n+1):
-    alphabet = [0]*26
-    s = str(input())
-    s = s.lower()
+x, m = map(int, input().split())
 
-    for i in s:
-        if i.isalpha():
-            alphabet[ord(i)-97] += 1
+mp = x
+for i in range(m):
+    mp += x
 
-    minNum = int(1e6)
-    for i in alphabet:
-        minNum = min(minNum, i)
-
-    print(f'Case {test_case}:', end=' ')
-    if minNum == 0:
-        print('Not a pangram')
-    elif minNum == 1:
-        print('Pangram!')
-    elif minNum == 2:
-        print('Double pangram!!')
-    else:
-        print('Triple pangram!!!')
+print(mp)
