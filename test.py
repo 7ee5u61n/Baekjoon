2f8bc17643d384b2b445669838760abe8d4c807a
7ee5u61n <le2471@gmail.com>
2025-02-25 18:51:50 +0900

    빙글빙글 스네일 해결

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-x, m = map(int, input().split())
+n = int(input())
 
-mp = x
-for i in range(m):
-    mp += x
+x = 0
+y = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+lenth = 0
+time = 0
+for i in range(n):
+    if i % 2 == 0:
+        lenth += 1
+    for j in range(lenth):
+        x += dx[i%4]
+        y += dy[i%4]
+        time += 1
+        if time == n:
+            break
+    if time == n:
+        break
 
-print(mp)
+print(x, y)
