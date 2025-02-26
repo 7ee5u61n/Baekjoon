c5ccda3be360d22dbc42fbf59a0c582ce3e261df
7ee5u61n <le2471@gmail.com>
2025-02-26 19:38:56 +0900

    별 찍기 - 9 해결

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
 n = int(input())
 
-x = 0
-y = 0
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-lenth = 0
-time = 0
-for i in range(n):
-    if i % 2 == 0:
-        lenth += 1
-    for j in range(lenth):
-        x += dx[i%4]
-        y += dy[i%4]
-        time += 1
-        if time == n:
-            break
-    if time == n:
-        break
-
-print(x, y)
+for i in range(n-1):
+    print(' '*i+'*'*(2*n-1-2*i))
+print(' '*(n-1)+'*')
+for i in range(n-2, -1, -1):
+    print(' '*i+'*'*(2*n-1-2*i))
