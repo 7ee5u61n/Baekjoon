87aad8d16c57c723d5b1eabc1171216ca63df621
7ee5u61n <le2471@gmail.com>
2025-03-02 20:46:54 +0900

    거리의 합 해결

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-s = str(input())
-# 25자 이내
-if n <= 25:
-    print(s)
-else:
-    rest = s[11:-11]
-    # 한 문장 안
-    if '.' not in rest[:-1]:
-        rest = '...'
-        print(s[:11]+rest+s[-11:])
-    else:
-        rest = '......'
-        print(s[:9]+rest+s[-10:])
+x = list(map(int, input().split()))
+
+lenth = 0
+for i in range(n):
+    for j in range(n):
+        lenth += abs(x[i]-x[j])
+
+print(lenth)
