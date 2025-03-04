e00c3c2317aba1d0ca8380f7a64de2ca022e2bf4
7ee5u61n <le2471@gmail.com>
2025-03-04 20:45:50 +0900

    문어 숫자 해결

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import math
+octopus = {'-':0, '\\':1, '(':2, '@':3, '?':4, '>':5, '&':6, '%':7, '/':-1}
 
-d, h, w = map(int, input().split())
+while True:
+    s = str(input())
+    if s == '#':
+        break
+    value = 0
+    for i in range(len(s)):
+        value += octopus[s[i]]*(8**(len(s)-i-1))
 
-value = d/math.sqrt(h**2+w**2)
-rH = int(h*value)
-rW = int(w*value)
-
-print(rH, rW)
+    print(value)
