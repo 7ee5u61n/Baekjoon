1d0a82747ad23d61ab3e315f9ed1c5480699b637
7ee5u61n <le2471@gmail.com>
2025-03-05 21:20:55 +0900

    시험 점수 해결

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-octopus = {'-':0, '\\':1, '(':2, '@':3, '?':4, '>':5, '&':6, '%':7, '/':-1}
+s = list(map(int, input().split()))
+t = list(map(int, input().split()))
 
-while True:
-    s = str(input())
-    if s == '#':
-        break
-    value = 0
-    for i in range(len(s)):
-        value += octopus[s[i]]*(8**(len(s)-i-1))
-
-    print(value)
+if sum(s) >= sum(t):
+    print(sum(s))
+else:
+    print(sum(t))
