5f286c8fe5e5ec77377929c6d81530356bcf757c
7ee5u61n <le2471@gmail.com>
2025-03-07 18:14:32 +0900

    등차수열의 합 해결

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-T = int(input())
-for _ in range(T):
-    h, w = map(int, input().split())
-    for _ in range(h):
-        meat = input()
-        print(meat[::-1])
+n = int(input())
+a = list(map(int, input().split()))
+
+arithmetical = True
+if n > 2:
+    degree = a[1] - a[0]
+    for i in range(1, n-1):
+        if a[i+1] - a[i] != degree:
+            arithmetical = False
+            break
+
+if arithmetical:
+    b = a
+    c = [0]*n
+    print('YES')
+    print(*b)
+    print(*c)
+else:
+    print('NO')
