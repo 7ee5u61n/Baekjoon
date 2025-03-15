e8bf954f5ab34d26e055e812a8699765d46b765b
7ee5u61n <le2471@gmail.com>
2025-03-15 18:04:14 +0900

    문자열 압축 해제 해결

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-arr = list(map(int, input().split()))
+n = int(input())
+compression = dict()
 
-if arr == sorted(arr):
-    print('Good')
-else:
-    print('Bad')
+for _ in range(n):
+    lower, upper = map(str, input().split())
+    compression[upper] = lower
+
+packed = str(input())
+unpacked = ''
+for i in packed:
+    unpacked += compression[i]
+
+s, e = map(int, input().split())
+print(unpacked[s-1:e])
