84e42ebd10401855ade5f3fa3b4f469e380ec8e7
7ee5u61n <le2471@gmail.com>
2025-03-17 20:54:50 +0900

    해커톤 해결

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
-n1, n2 = map(int, input().split())
-group1 = list(input())
-group2 = list(input())
-t = int(input())
+n = int(input())
+x = list(map(int, input().split()))
+x = sorted(x)
 
-group1.reverse()
-sequence = group1 + group2
+team = 0
+limit = x[0]
+member = 0
+for i in range(n):
+    if member == 0:
+        limit = x[i]
+    member += 1
 
-for _ in range(t):
-    for i in range(len(sequence)-1):
-        if sequence[i] in group1 and sequence[i+1] in group2:
-            sequence[i], sequence[i+1] = sequence[i+1], sequence[i]
-            
-            if sequence[i+1] == group1[-1]:
-                break
+    if member == limit:
+        team += 1
+        member = 0
 
-for i in range(n1+n2):
-    print(sequence[i], end='')
+if member:
+    team += 1
+
+print(team)
