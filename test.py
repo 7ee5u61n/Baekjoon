b88ff6c92b922f56a1317a0257b9c30ceb7ae1f9
7ee5u61n <le2471@gmail.com>
2025-04-03 20:49:52 +0900

    괄호 없는 사칙연산 해결

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-n = int(input())
-print(n-1946)
+def calc(a, b, c):
+    value = 0
+    if b == '+':
+        value += a+c
+    elif b == '-':
+        value += a-c
+    elif b == '*':
+        value += a*c
+    elif b == '/':
+        value += int(a/c)
+
+    return value
+
+ko = list(map(str, input().split()))
+
+k1 = int(ko[0])
+o1 = ko[1]
+k2 = int(ko[2])
+o2 = ko[3]
+k3 = int(ko[4])
+
+value1 = calc(calc(k1, o1, k2), o2, k3)
+value2 = calc(k1, o1, calc(k2, o2, k3))
+
+print(min(value1, value2))
+print(max(value1, value2))
