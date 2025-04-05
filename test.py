e25b38264ddbea9fba65bd45da04b2b4ff81f13d
7ee5u61n <le2471@gmail.com>
2025-04-05 17:54:15 +0900

    Binary Counting 해결

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-for a in range(1, 10):
-    # A가 a인지 물어보고 flush를 한다.
-    # print에 flush 파라미터를 넣으면 된다.
-    print("? A", a, flush=True)
-    
-    # 채점기의 답변을 입력받는다.
-    resp = int(input())
+n, k = map(int, input().split())
 
-    if resp == 1:
-        # 답변이 "예"이므로 A의 값은 a이다.
-        # B의 값도 알아내야 하는데, 이 부분은 직접 채워보자.
-        for b in range(1, 10):
-            print("? B", b, flush=True)
-            resp = int(input())
+num = ''
+for i in range(n*5):
+    num += format(i, 'b')
 
-            if resp == 1:
-                print("!", a + b)
-                break
-        break
+for i in range(5):
+    print(num[(k+i*n-1)], end=' ')
