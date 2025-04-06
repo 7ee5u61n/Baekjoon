0e9611dcb55d9586b57143cdf0150abd127dee74
7ee5u61n <le2471@gmail.com>
2025-04-06 19:45:43 +0900

    햄버거 해결

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
-n, k = map(int, input().split())
+n = int(input())
+A, B, C, D = map(int, input().split())
+s = input()
 
-num = ''
-for i in range(n*5):
-    num += format(i, 'b')
+stack = 0
+if s[0] == 'a' and s[-1] == 'a':
+    for i in range(n):
+        if s[i] == 'a':
+            A -= 1
+        elif s[i] == 'b':
+            B -= 1
+        elif s[i] == 'c':
+            C -= 1
+        elif s[i] == 'd':
+            D -= 1
 
-for i in range(5):
-    print(num[(k+i*n-1)], end=' ')
+        if i != n-1:
+            if s[i] == s[i+1]:
+                break
+
+        stack += 1
+
+if stack == n and A >= 0 and B >= 0 and C >= 0 and D >= 0:
+    print('Yes')
+else:
+    print('No')
