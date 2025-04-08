a122181026a0f9e75925c92d57d716a788f4a4f9
7ee5u61n <le2471@gmail.com>
2025-04-08 20:14:16 +0900

    회전 해결

--- test.py
+++ test.py
@@ -1,28 +1,14 @@
-n , m = map(int, input().split())
+from collections import deque
 
-entry = list(0 for _ in range(n+1))
-blue = []
-white = []
+n = deque(input())
+first = deque(n)
 
+result = 0
 while True:
-    classNumber, studentName = input().split()
-    if classNumber == '0' and studentName == '0':
+    n.appendleft(n.pop())
+    result += int(''.join(n))
+
+    if n == first:
         break
 
-    classNumber = int(classNumber)
-
-    if entry[classNumber] < m:
-        if classNumber % 2 == 1:
-            blue.append((classNumber, studentName))
-        else:
-            white.append((classNumber, studentName))
-
-        entry[classNumber] += 1
-
-blue.sort(key= lambda x: (x[0], len(x[1]), x[1]))
-white.sort(key= lambda x: (x[0], len(x[1]), x[1]))
-
-result = blue + white
-
-for i in result:
-    print(*i)
+print(result)
