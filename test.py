a450327fcf548eda4d48f4f8b1dcbc78ccba176c
7ee5u61n <le2471@gmail.com>
2025-04-16 18:50:22 +0900

    A+B - 2 해결

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-dp = [0]*65
-dp[1] = 1
+a = int(input())
+b = int(input())
 
-for i in range(2, 65):
-    dp[i] = dp[i-1]*2
-
-n = int(input())
-
-count = 0
-for i in range(65):
-    if n <= dp[i]:
-        count = i
-        n -= dp[i]
-        break
-
-print(count)
+print(a+b)
