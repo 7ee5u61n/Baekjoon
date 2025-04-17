4743295917bdf7da4c578a050cffdd4ebd093426
7ee5u61n <le2471@gmail.com>
2025-04-17 20:01:17 +0900

    Gift Expire Date 해결

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-a = int(input())
-b = int(input())
+today = list(map(int, input().split('-')))
+n = int(input())
 
-print(a+b)
+usable = 0
+for _ in range(n):
+    expire = list(map(int, input().split('-')))
+    if today[0] < expire[0]:
+        usable += 1
+        continue
+    elif today[0] == expire[0]:
+        if today[1] < expire[1]:
+            usable += 1
+            continue
+        elif today[1] == expire[1]:
+            if today[2] <= expire[2]:
+                usable += 1
+                continue
+
+print(usable)
