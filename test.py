44040824d1198de5fdd419ecf6352e524d4c89c1
7ee5u61n <le2471@gmail.com>
2025-04-18 19:05:58 +0900

    나머지와 몫이 같은 수 해결

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-today = list(map(int, input().split('-')))
 n = int(input())
 
-usable = 0
-for _ in range(n):
-    expire = list(map(int, input().split('-')))
-    if today[0] < expire[0]:
-        usable += 1
-        continue
-    elif today[0] == expire[0]:
-        if today[1] < expire[1]:
-            usable += 1
-            continue
-        elif today[1] == expire[1]:
-            if today[2] <= expire[2]:
-                usable += 1
-                continue
+result = 0
+for i in range(n):
+    result += n*i+i
 
-print(usable)
+print(result)
