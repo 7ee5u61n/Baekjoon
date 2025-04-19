6edad0ff566422e9e141c627033839797cab3eb6
7ee5u61n <le2471@gmail.com>
2025-04-19 21:13:15 +0900

    뚜기뚜기메뚜기  해결

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-n = int(input())
+keypad = [[''], ['a', 'b', 'c'], ['d', 'e', 'f'],
+          ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
+          ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
 
-result = 0
-for i in range(n):
-    result += n*i+i
+arr = list(map(int, input().split()))
+s = str(input())
 
-print(result)
+key = [[] for _ in range(9)]
+for i in range(9):
+    key[i] = keypad[arr[i]-1]
+
+output = '#'
+for i in range(len(s)):
+    for j in range(9):
+        if s[i] in key[j]:
+            temp = (str(j+1)*(key[j].index(s[i])+1))
+            if output[-1] == temp[0]:
+                print('#'+temp, end='')
+            else:
+                print(temp, end='')
+            output = temp
+            break
