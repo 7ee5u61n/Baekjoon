de31fd5b4fd67b0a974170cc434509401cf5ddec
7ee5u61n <le2471@gmail.com>
2025-04-21 20:08:10 +0900

    전투의 신 해결

--- test.py
+++ test.py
@@ -1,25 +1,13 @@
-upperVowel = ['A', 'I', 'Y', 'E', 'O', 'U']
-upperConsonant = ['B', 'K', 'X', 'Z', 'N', 'H', 'D', 'C', 'W', 'G', 'P', 'V', 'J', 'Q', 'T', 'S', 'R', 'L', 'M', 'F']
-lowerVowel = ['a', 'i', 'y', 'e', 'o', 'u']
-lowerConsonant = ['b', 'k', 'x', 'z', 'n', 'h', 'd', 'c', 'w', 'g', 'p', 'v', 'j', 'q', 't', 's', 'r', 'l', 'm', 'f']
+n = int(input())
+a, pa, b, pb = map(int, input().split())
 
-while True:
-    try:
-        s = str(input())    
-        for i in range(len(s)):
-            if s[i].isupper():
-                if s[i] in upperVowel:
-                    print(upperVowel[(upperVowel.index(s[i])+3)%6], end='')
-                else:
-                    print(upperConsonant[(upperConsonant.index(s[i])+10)%20], end='')
-            elif s[i].islower():
-                if s[i] in lowerVowel:
-                    print(lowerVowel[(lowerVowel.index(s[i])+3)%6], end='')
-                else:
-                    print(lowerConsonant[(lowerConsonant.index(s[i])+10)%20], end='')
+tank = 0
+dps = 0
 
-            else:
-                print(s[i], end='')
-        print()
-    except EOFError:
-        break
+for x in range(n//pa+1):
+    y = (n-pa*x)//pb
+    if a*tank + b*dps < a*x+b*y:
+        tank = x
+        dps = y
+
+print(tank, dps)
