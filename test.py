afd433ac6ee863877fd5f3796e89b55b92952e68
7ee5u61n <le2471@gmail.com>
2025-04-22 19:49:10 +0900

    블로그2 해결

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 n = int(input())
-a, pa, b, pb = map(int, input().split())
+s = str(input())
 
-tank = 0
-dps = 0
+color = {'R':0, 'B':0}
+color[s[0]] = 1
 
-for x in range(n//pa+1):
-    y = (n-pa*x)//pb
-    if a*tank + b*dps < a*x+b*y:
-        tank = x
-        dps = y
+for i in range(1, n):
+    if s[i-1] != s[i]:
+        color[s[i]] += 1
 
-print(tank, dps)
+result = min(color['R'], color['B']) + 1
+
+print(result)
