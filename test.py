1cf005828166f77e93cfc28794079c884e5e3f81
7ee5u61n <le2471@gmail.com>
2025-04-25 18:22:59 +0900

    몬스터 트럭 해결

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-n = str(input())
+def dfs(x, y):
+    dx = [0, 0, 1, 1]
+    dy = [0, 1, 0, 1]
 
-result = 0
-for i in n:
-    result += int(i)**5
+    crash = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if map[nx][ny] == 'X':
+            crash += 1
+        elif map[nx][ny] == '#':
+            return
+    
+    park[crash] += 1
 
-print(result)
+r, c = map(int, input().split())
+map = list(list(input()) for _ in range(r))
+
+park = [0]*5
+for i in range(r-1):
+    for j in range(c-1):
+        dfs(i, j)
+
+for i in park:
+    print(i)
