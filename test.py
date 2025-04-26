873743f5f8acfdc97c05008dcbc45aabbff9c3c4
7ee5u61n <le2471@gmail.com>
2025-04-26 18:13:17 +0900

    오셀로 재배치 해결

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
-def dfs(x, y):
-    dx = [0, 0, 1, 1]
-    dy = [0, 1, 0, 1]
+T = int(input())
+for _ in range(T):
+    n = int(input())
 
-    crash = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if map[nx][ny] == 'X':
-            crash += 1
-        elif map[nx][ny] == '#':
-            return
-    
-    park[crash] += 1
+    base = list(input())
+    goal = list(input())
 
-r, c = map(int, input().split())
-map = list(list(input()) for _ in range(r))
+    wrongB = 0
+    wrongW = 0
 
-park = [0]*5
-for i in range(r-1):
-    for j in range(c-1):
-        dfs(i, j)
+    for i in range(n):
+        if base[i] != goal[i]:
+            if base[i] == 'B':
+                wrongB += 1
+            else:
+                wrongW += 1
 
-for i in park:
-    print(i)
+    if wrongB >= wrongW:
+        result = wrongB
+    else:
+        result = wrongW
+
+    print(result)
