5ec9c237c2690056b1717adb61ec8074b8ff046c
7ee5u61n <le2471@gmail.com>
2025-04-27 19:46:04 +0900

    미터계 해결

--- test.py
+++ test.py
@@ -1,23 +1,12 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
+n = str(input())
 
-    base = list(input())
-    goal = list(input())
+number = [0, 1, 2, 3, 5, 6, 7, 8, 9]
 
-    wrongB = 0
-    wrongW = 0
-
-    for i in range(n):
-        if base[i] != goal[i]:
-            if base[i] == 'B':
-                wrongB += 1
-            else:
-                wrongW += 1
-
-    if wrongB >= wrongW:
-        result = wrongB
+dec = 0
+for i in range(len(n)):
+    if int(n[i]) > 4:
+        dec += (int(n[i])-1)*(9**(len(n)-i-1))
     else:
-        result = wrongW
+        dec += int(n[i])*(9**(len(n)-i-1))
 
-    print(result)
+print(dec)
