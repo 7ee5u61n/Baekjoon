3cc30ba2fd93b8d0337a78a1df3f3f497b02214a
7ee5u61n <le2471@gmail.com>
2025-04-28 18:53:45 +0900

    이제는 더 이상 물러날 곳이 없다 해결

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-n = str(input())
+n = int(input())
 
-number = [0, 1, 2, 3, 5, 6, 7, 8, 9]
-
-dec = 0
-for i in range(len(n)):
-    if int(n[i]) > 4:
-        dec += (int(n[i])-1)*(9**(len(n)-i-1))
-    else:
-        dec += int(n[i])*(9**(len(n)-i-1))
-
-print(dec)
+if n % 2 == 0:
+    print('Duck')
+else:
+    print('Goose')
