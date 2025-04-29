0f47636efb662a87fb75ca5ad17c19d96d970884
7ee5u61n <le2471@gmail.com>
2025-04-29 21:20:51 +0900

    일곱  난쟁이 해결

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-n = int(input())
+dwarf = [int(input()) for _ in range(9)]
+dwarf.sort()
+rest = sum(dwarf)-100
 
-if n % 2 == 0:
-    print('Duck')
-else:
-    print('Goose')
+for i in range(9):
+    if len(dwarf) == 9:
+        for j in range(i, 9):
+            if dwarf[i]+dwarf[j] == rest:
+                dwarf.pop(i)
+                dwarf.pop(j-1)
+                break
+    
+for i in range(7):
+    print(dwarf[i])
