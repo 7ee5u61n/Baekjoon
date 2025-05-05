050ba19eb8866990313a218e0c60ccb84e2ef936
7ee5u61n <le2471@gmail.com>
2025-05-05 21:18:09 +0900

    쿠키의 신체 측정 해결

--- test.py
+++ test.py
@@ -1,20 +1,38 @@
-n, m = map(int, input().split())
+n = int(input())
+arr = [list(input()) for _ in range(n)]
 
-lecture = {}
-
+# 심장 위치
+row = 0 
+col = 0
 for i in range(n):
-    k = int(input())
-    student = list(map(int, input().split()))
+    if row != 0 and col != 0:
+        break
+    for j in range(n):
+        if arr[i][j] == '*':
+            row = i+1
+            col = j
+            break
 
-    for j in range(k):
-        if student[j] in lecture:
-            lecture[student[j]] += 1
-        else:
-            lecture[student[j]] = 1
+leftArm = 0
+rightArm = 0
+waist = 0
+leftLeg = 0
+rightLeg = 0
 
-result = 0
-for k, v in lecture.items():
-    if v >= m:
-        result += 1
-
-print(result)
+for i in range(1, n):
+    if (0 <= row < n) and (0 <= col-i < n) and arr[row][col-i] == '*':
+        leftArm += 1
+    
+    if (0 <= row < n) and (0 <= col+i < n) and arr[row][col+i] == '*':
+        rightArm += 1
+    
+    if (0 <= row+i < n) and (0 <= col < n) and arr[row+i][col] == '*':
+        waist += 1
+    elif (0 <= row+i < n) and (0 <= col < n) and arr[row+i][col] == '_':
+        if arr[row+i][col-1] == '*':
+            leftLeg += 1
+        if arr [row+i][col+1] == '*':
+            rightLeg += 1  
+        
+print(row+1, col+1)
+print(leftArm, rightArm, waist, leftLeg, rightLeg)
