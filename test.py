1f8f7c6c99319e9b3568242d94306cf8335a57a1
7ee5u61n <le2471@gmail.com>
2025-05-08 10:12:43 +0900

    비밀편지 해결

--- test.py
+++ test.py
@@ -1,3 +1,33 @@
-s = 'WelcomeToSMUPC'
+def match(a):
+    global returnValue
+    highRate = 0
+    for i in range(8):
+        rate = 0
+        for j in range(6):
+            if a[j] == letter[i][1][j]:
+                rate += 1
+        if rate > highRate:
+            highRate = rate
+            returnValue = letter[i][0]
+    if highRate >= 5:
+        return True
+    else:
+        return False
+
+letter = [['A', '000000'], ['B', '001111'], ['C', '010011'], ['D', '011100'], 
+          ['E', '100110'], ['F', '101001'], ['G', '110101'], ['H', '111010']]
+
 n = int(input())
-print(s[(n-1)%len(s)])
+s = str(input())
+
+result = ''
+for i in range(n):
+    a = s[i*6:(i*6)+6]
+    returnValue = ''
+    if match(a):
+        result += returnValue
+    else:
+        result = i+1
+        break
+
+print(result)
