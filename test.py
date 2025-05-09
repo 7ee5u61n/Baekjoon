044bdc4efb6d59169795a6e0920f7dc8f03baca2
7ee5u61n <le2471@gmail.com>
2025-05-09 22:47:51 +0900

    뒤집기 해결

--- test.py
+++ test.py
@@ -1,33 +1,13 @@
-def match(a):
-    global returnValue
-    highRate = 0
-    for i in range(8):
-        rate = 0
-        for j in range(6):
-            if a[j] == letter[i][1][j]:
-                rate += 1
-        if rate > highRate:
-            highRate = rate
-            returnValue = letter[i][0]
-    if highRate >= 5:
-        return True
+n, k, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+for _ in range(m):
+    i = int(input())
+    if i > 0:
+        if i >= k:
+            k = i-k+1
     else:
-        return False
+        if i < k-n:
+            k = i+n-(k-n-1)
 
-letter = [['A', '000000'], ['B', '001111'], ['C', '010011'], ['D', '011100'], 
-          ['E', '100110'], ['F', '101001'], ['G', '110101'], ['H', '111010']]
-
-n = int(input())
-s = str(input())
-
-result = ''
-for i in range(n):
-    a = s[i*6:(i*6)+6]
-    returnValue = ''
-    if match(a):
-        result += returnValue
-    else:
-        result = i+1
-        break
-
-print(result)
+print(k)
