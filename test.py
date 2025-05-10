303ca569ce1d5dff99c3272c228bf8a13296b7d4
7ee5u61n <le2471@gmail.com>
2025-05-10 19:06:29 +0900

    도비의 난독증 테스트 해결

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-n, k, m = map(int, input().split())
-a = list(map(int, input().split()))
-
-for _ in range(m):
-    i = int(input())
-    if i > 0:
-        if i >= k:
-            k = i-k+1
-    else:
-        if i < k-n:
-            k = i+n-(k-n-1)
-
-print(k)
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    
+    arr = [str(input()) for _ in range(n)]
+    arr.sort(key= lambda x: x.upper())
+    
+    print(arr[0])
