ff4da6a77020bdf0357184abc2e0711f5b4a78c8
7ee5u61n <le2471@gmail.com>
2025-05-14 19:55:32 +0900

    A+B - 9 해결

--- test.py
+++ test.py
@@ -1,26 +1,5 @@
-j = int(input())
-a = int(input())
+import sys
+input = sys.stdin.readline
 
-jersey = ['X']
-for i in range(j):
-    jersey.append(str(input()))
-
-result = 0
-for i in range(a):
-    size, number = input().split()
-    number = int(number)
-
-    if jersey[number] != 'X':
-        if size == 'S':
-            jersey[number] = 'X'
-            result += 1
-        elif size == 'M':
-            if jersey[number] != 'S':
-                jersey[number] = 'X'
-                result += 1
-        else:
-            if jersey[number] == 'L':
-                jersey[number] = 'X'
-                result += 1
-
-print(result)
+a, b = map(int, input().split())
+print(a+b)
