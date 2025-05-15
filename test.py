626ac73c49f007164e1a3f03c13e5be20b46ff19
7ee5u61n <le2471@gmail.com>
2025-05-15 21:12:06 +0900

    2017 아주대학교 프로그래밍 경시대회 (Small) 해결

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-a, b = map(int, input().split())
-print(a+b)
+player = []
+for i in range(1, n+1):
+    s, c, l = map(int, input().split())
+    player.append([s, c, l, i])
+
+player.sort(key= lambda x:(-x[0], x[1], x[2]))
+
+print(player[0][3])
