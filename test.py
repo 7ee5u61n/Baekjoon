dd078b2d315af2ed51908922eaea44e01baff019
7ee5u61n <le2471@gmail.com>
2025-05-16 12:40:44 +0900

    이진수 합 해결

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-n = int(input())
+k = int(input())
 
-player = []
-for i in range(1, n+1):
-    s, c, l = map(int, input().split())
-    player.append([s, c, l, i])
-
-player.sort(key= lambda x:(-x[0], x[1], x[2]))
-
-print(player[0][3])
+print('1'*k+'0'*(k-1))
