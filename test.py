71d48884c71d393661bf4a777aa0b9891998a018
7ee5u61n <le2471@gmail.com>
2025-05-17 22:30:57 +0900

    오늘도 졌다 해결

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-k = int(input())
+geminis = list(map(int, input().split()))
+gullivers = list(map(int, input().split()))
+a = 0
+b = 0
+counter = False
+for i in range(9):
+    a += geminis[i]
+    if a > b:
+        counter = True
+    b += gullivers[i]
+    if a > b:
+        counter = True
 
-print('1'*k+'0'*(k-1))
+if counter and a < b:
+    print('Yes')
+else:
+    print('No')
