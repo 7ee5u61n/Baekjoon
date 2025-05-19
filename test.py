047931f3639dd8bede7d9039640c1dd57003215f
7ee5u61n <le2471@gmail.com>
2025-05-19 20:57:01 +0900

    카드 게임 해결

--- test.py
+++ test.py
@@ -1,39 +1,6 @@
-from collections import deque
+result = 0
 
-n = int(input())
-a = []
-b = []
-c = []
-waiting = deque()
+for _ in range(5):
+    result += int(input())
 
-for _ in range(n):
-    s = list(map(int, input().split()))
-
-    if s[0] == 1:
-        waiting.append([s[1], s[2]])
-    else:
-        student, menu = waiting.popleft()
-        
-        if menu == s[1]:
-            a.append(student)
-        else:
-            b.append(student)
-
-for i in waiting:
-    c.append(i[0])
-
-if a:
-    a.sort()
-    print(*a)
-else:
-    print('None')
-if b:
-    b.sort()
-    print(*b)
-else:
-    print('None')
-if c:
-    c.sort()
-    print(*c)
-else:
-    print('None')
+print(result)
