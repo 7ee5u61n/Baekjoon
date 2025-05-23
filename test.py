f4b22b239a3d5e0f1e9a515574afca7556026d16
7ee5u61n <le2471@gmail.com>
2025-05-23 17:18:39 +0900

    알고리즘 수업 - 삽입 정렬 1 해결

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 a = list(map(int, input().split()))
+count = 0
+result = -1
+for i in range(1, n):
+    loc = i-1
+    newItem = a[i]
 
-diet = 0
-day = -1
-for i in range(n-1, -1, -1):
-    diet += a[i]
-    if diet >= m:
-        day = i+1
-        break
-        
-print(day)
+    while loc >= 0 and newItem < a[loc]:
+        count += 1
+        a[loc+1] = a[loc]
+        loc -= 1
+
+        if count == k:
+            result = a[loc+1]
+            break
+
+    if (loc+1 != i):
+        count += 1
+        a[loc+1] = newItem
+            
+        if count == k:
+            result = a[loc+1]
+            break
+
+print(result)
