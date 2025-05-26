0e912531ec6a68bd740789735b9edb7945f57502
7ee5u61n <le2471@gmail.com>
2025-05-26 18:55:55 +0900

    박스 나누기 게임 해결

--- test.py
+++ test.py
@@ -1,37 +1,6 @@
-import sys
-input = sys.stdin.readline
+n, m = map(int, input().split())
 
-n = int(input())
-
-queue = []
-for i in range(1, n+1):
-    order = list(input().split())
-    if order[0] == 'push':
-        queue.append(order[1])
-    
-    elif order[0] == 'pop':
-        if queue:
-            print(queue.pop(0))
-        else:
-            print(-1)
-
-    elif order[0] == 'size':
-        print(len(queue))
-
-    elif order[0] == 'empty':
-        if queue:
-            print(0)
-        else:
-            print(1)
-
-    elif order[0] == 'front':
-        if queue:
-            print(queue[0])
-        else:
-            print(-1)
-
-    elif order[0] == 'back':
-        if queue:
-            print(queue[-1])
-        else:
-            print(-1)
+if n % 2 == 0 or m % 2 == 0:
+    print('A')
+else:
+    print('B')
