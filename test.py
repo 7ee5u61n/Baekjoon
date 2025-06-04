3f2539d7c613a244cbe1f61c9c5db277bfa801c0
7ee5u61n <le2471@gmail.com>
2025-06-04 18:18:00 +0900

    2의 제곱수 계산하기 해결

--- test.py
+++ test.py
@@ -1,32 +1,11 @@
 n = int(input())
 
-for i in range(n):
-    usG, usS, usB, rusG, rusS, rusB = map(int, input().split())
-
-    count = False
-    color = False
-    
-    if usG+usS+usB > rusG+rusS+rusB:
-        count = True
-    
-    if usG > rusG:
-        color = True
-    elif usG == rusG:
-        if usS > rusS:
-            color = True
-        elif usS == rusS:
-            if usB > rusB:
-                color = True
-
-    print(usG, usS, usB, rusG, rusS, rusB)
-    
-    if count and color:
-        print('both')
-    elif count:
-        print('count')
-    elif color:
-        print('color')
+k = 64
+while True:
+    if n%2 == 1:
+        break
     else:
-        print('none')
-    
-    print()
+        n//=2
+    k -= 1
+
+print(k)
