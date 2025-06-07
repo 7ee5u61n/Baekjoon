44b82611b53565bb08cbba825c3dea35cca8ef65
7ee5u61n <le2471@gmail.com>
2025-06-07 19:07:23 +0900

    유미 해결

--- test.py
+++ test.py
@@ -1,9 +1,35 @@
-import sys
-input = sys.stdin.readline
+import math
 
-n = int(input())
-arr = list(int(input()) for _ in range(n))
+coord = [dict() for _ in range(4)]
+dist = [[0.0] * 4 for _ in range(4)]
+visit = [False] * 4
+result = int(1e6)
 
-arr = sorted(arr)
-for i in range(n):
-    print(arr[i])
+def dfs(index, total, count):
+    global result
+    if count == 4:
+        result = min(result, int(total))
+        return
+    
+    if visit[index]:
+        return
+    
+    visit[index] = True
+    for i in range(4):
+        dfs(i, total + dist[index][i], count + 1)
+    visit[index] = False
+
+for i in range(4):
+    x, y = map(int, input().split())
+    coord[i]['x'], coord[i]['y'] = x, y
+
+for i in range(4):
+    x, y = coord[i]['x'], coord[i]['y']
+    for j in range(i + 1, 4):
+        nx, ny = coord[j]['x'], coord[j]['y']
+        d = math.sqrt((x - nx) ** 2 + (y - ny) ** 2)
+        dist[i][j] = dist[j][i] = d
+
+dfs(0, 0.0, 0)
+
+print(result)
