b9b8cdbb26f88c1cbb5d726525b93c0815c9eec4
7ee5u61n <le2471@gmail.com>
2025-06-11 14:06:33 +0900

    Do Not Touch Anything 해결

--- test.py
+++ test.py
@@ -1,27 +1,14 @@
-n = int(input())
+r, c, n = map(int, input().split())
 
-# 약속 기록
-record = [list(input().split()) for _ in range(n)]
-# 0: 약속 없음
-plan = [0]*70
+result = 1
+if r%n == 0:
+    result *= r//n
+else:
+    result *= r//n+1
 
-for i in range(n):
-    name, m = input().split()
-    for s, w, d, p in record:
-        if name == s:
-            if int(m) >= int(p):
-                day = (int(w)-1)*7+int(d)
-                plan[day] += 1
-                break
-            break
-    
-result = 0
-value = 0
-for i in range(70):
-    if plan[i] == 0:
-        value = 0
-    else:
-        value += 1
-    result = max(value, result)
+if c%n == 0:
+    result *= c//n
+else:
+    result *= c//n+1
 
 print(result)
