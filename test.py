f4b7229cb1d278ab33dfabf5d8381b53289f9e73
7ee5u61n <le2471@gmail.com>
2025-06-13 21:16:32 +0900

    유니대전 퀴즈쇼 해결

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
-s = list(input())
-t = int(input())
-for _ in range(t):
-    a, b = map(int, input().split())
-    s[a], s[b] = s[b], s[a]
+n, s = map(str, input().split())
 
-for i in s:
-    print(i, end='')
+# 채팅 기록
+arr = []
+# 정답
+answer = ''
+for i in range(int(n)):
+    name, chat = map(str, input().split())
+    arr.append([name, chat])
+    if name == s:
+        answer = chat
+
+# 당첨자 이전에 정답인 사람
+count = 0
+for name, chat in arr:
+    if chat == answer:
+        # 당첨자
+        if name == s:
+            break
+        count += 1
+        
+print(count)
