bf0fa038558aefe50b9c4ff59bb2c0ec3fd19f51
7ee5u61n <le2471@gmail.com>
2025-06-16 18:39:30 +0900

    정수 직사각형 해결

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
-import sys
-
-n = int(input())
-participant = {}
-for i in range(n):
-    name = str(input())
-    if participant.get(name):
-        participant[name] += 1
-    else:
-        participant[name] = 1
-
-for i in range(n-1):
-    name = str(input())
-    participant[name] -= 1
-
-for i, j in participant.items():
-    if j == 1:
-        print(i)
+while True:
+    h, w = map(int, input().split())
+    if h == 0 and w == 0:
         break
+    square = []
+    for i in range(1, 151):
+        for j in range(i, 151):
+            if i == j:
+                continue
+            else:
+                square.append([(i**2+j**2), i, j])
+
+    square = sorted(square, key=lambda x: (x[0], x[1]))
+    result = square[square.index([h**2+w**2, h, w])+1]
+    print(result[1], result[2])
