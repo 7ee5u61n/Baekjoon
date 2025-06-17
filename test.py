1179b9831b2baa6e7b23c86e60700bf7b25ec649
7ee5u61n <le2471@gmail.com>
2025-06-17 19:04:45 +0900

    헛간 청약 해결

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-while True:
-    h, w = map(int, input().split())
-    if h == 0 and w == 0:
-        break
-    square = []
-    for i in range(1, 151):
-        for j in range(i, 151):
-            if i == j:
-                continue
-            else:
-                square.append([(i**2+j**2), i, j])
-
-    square = sorted(square, key=lambda x: (x[0], x[1]))
-    result = square[square.index([h**2+w**2, h, w])+1]
-    print(result[1], result[2])
+n, w, h, l = map(int, input().split())
+print(min(n, (w // l) * (h // l)))
