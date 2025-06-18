1cbd12d64734e3aa8a4423f7438e70c40a6e4d10
7ee5u61n <le2471@gmail.com>
2025-06-18 23:58:06 +0900

    영리한 아리의 포탈 타기 해결

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-n, w, h, l = map(int, input().split())
-print(min(n, (w // l) * (h // l)))
+n, m = map(int, input().split())
+a, d = map(int, input().split())
+x, y = map(int, input().split())
+
+if x == n and n % 2 != d:
+    print('YES!')
+else:
+    print('NO...')
