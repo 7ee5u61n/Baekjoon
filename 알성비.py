cc0c6e107759cca2f68dbde813490b9c1254791f
7ee5u61n <le2471@gmail.com>
2025-06-20 22:55:05 +0900

    알성비 수정

--- 알성비.py
+++ 알성비.py
@@ -1,5 +1,10 @@
 print('가격: 원, 용량: ml, 도수: %. 순으로 입력')
 price = capacity = volume = 1
-while price != 0 and capacity != 0 and volume != 0:
+while True:
     price, capacity, volume = map(int, input().split())
-    print(capacity*volume/price)
+    if price and capacity and volume:
+        result = price / (capacity * volume)
+        print(f'가격: {price}원, 용량: {capacity}ml, 도수: {volume}%, 가격/알코올: {result:.2f}원/ml')
+    else:
+        print('종료합니다.')
+        break
