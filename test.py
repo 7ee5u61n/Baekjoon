091abadd9cf3cbf03844484cdb21a09d6c2a1b6b
7ee5u61n <le2471@gmail.com>
2025-06-23 20:52:58 +0900

    돈복사 해결

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
-m = int(input())
-cocktail = {}
+n = int(input())
+count = 0
+while True:
+    if len(str(n*2)) > len(str(n)):
+        break
 
-for i in range(m):
-    s, x = input().split()
-    x = int(x)
-    if s in cocktail:
-        cocktail[s] += x
-    else:
-        cocktail[s] = x
+    n *= 2
+    count += 1
 
-delicious = False
-for i in range(len(cocktail)):
-    for j in range(i + 1, len(cocktail)):
-        s1 = list(cocktail.values())[i]
-        s2 = list(cocktail.values())[j]
-        if int(s1*1.618) == s2 or int(s2*1.618) == s1:
-            delicious = True
-            break
-
-if delicious:
-    print("Delicious!")
-else:
-    print("Not Delicious...")
+print(count)
