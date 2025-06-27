8638bccbc79a3ccffa42a9d229278d6ee9a63177
7ee5u61n <le2471@gmail.com>
2025-06-27 23:53:54 +0900

    🐜 기적의 매매법 🐜 해결

--- test.py
+++ test.py
@@ -1,13 +1,40 @@
-import sys
-input = sys.stdin.readline
+m = int(input())
+jSeed = int(m)
+sSeed = int(m)
+jStock = 0
+sStock = 0
 
-n = int(input())
+price = list(map(int, input().split()))
 
-calc = ''
-for i in range(2*n-1):
-    value = str(input().strip())
-    if value == '/':
-        value = '//'
-    calc += value
+# 준현 매매법
+for i in range(14):
+    if jSeed // price[i] > 0:
+        jStock += jSeed // price[i]
+        jSeed -= (jSeed // price[i]) * price[i]
+# 남은거
+if jStock:
+    jSeed += jStock * price[13]
+    jStock = 0
 
-print(eval(calc))
+# 성민 매매법
+for i in range(3, 14):
+    # 3일 연속 증가
+    if sStock and price[i-3] < price[i-2] < price[i-1] < price[i]:
+        sSeed += sStock * price[i]
+        sStock = 0
+    # 3일 연속 감소
+    if price[i-3] > price[i-2] > price[i-1] > price[i]:
+        if sSeed // price[i] > 0:
+            sStock += sSeed // price[i]
+            sSeed -= (sSeed // price[i]) * price[i]
+# 남은거        
+if sStock:
+    sSeed += sStock * price[13]
+    sStock = 0
+
+if jSeed > sSeed:
+    print('BNP')
+elif jSeed < sSeed:
+    print('TIMING')
+else:
+    print('SAMESAME')
