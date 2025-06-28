57af43c80512f33747b205836bbe084d8e144a05
7ee5u61n <le2471@gmail.com>
2025-06-28 17:58:14 +0900

    인기 투표 해결

--- test.py
+++ test.py
@@ -1,40 +1,14 @@
-m = int(input())
-jSeed = int(m)
-sSeed = int(m)
-jStock = 0
-sStock = 0
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    candidate = list(int(input()) for _ in range(n))
+    maxVote = max(candidate)
 
-price = list(map(int, input().split()))
-
-# 준현 매매법
-for i in range(14):
-    if jSeed // price[i] > 0:
-        jStock += jSeed // price[i]
-        jSeed -= (jSeed // price[i]) * price[i]
-# 남은거
-if jStock:
-    jSeed += jStock * price[13]
-    jStock = 0
-
-# 성민 매매법
-for i in range(3, 14):
-    # 3일 연속 증가
-    if sStock and price[i-3] < price[i-2] < price[i-1] < price[i]:
-        sSeed += sStock * price[i]
-        sStock = 0
-    # 3일 연속 감소
-    if price[i-3] > price[i-2] > price[i-1] > price[i]:
-        if sSeed // price[i] > 0:
-            sStock += sSeed // price[i]
-            sSeed -= (sSeed // price[i]) * price[i]
-# 남은거        
-if sStock:
-    sSeed += sStock * price[13]
-    sStock = 0
-
-if jSeed > sSeed:
-    print('BNP')
-elif jSeed < sSeed:
-    print('TIMING')
-else:
-    print('SAMESAME')
+    if candidate.count(maxVote) > 1:
+        print('no winner')
+    else:
+        winner = candidate.index(maxVote) + 1
+        if maxVote > sum(candidate) / 2:
+            print(f'majority winner {winner}')
+        else:
+            print(f'minority winner {winner}')
