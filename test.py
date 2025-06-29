2a95757907d485a63ee3755ecdd2503cc673cc3c
7ee5u61n <le2471@gmail.com>
2025-06-29 13:26:59 +0900

    가장 긴 막대 자석 해결

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    candidate = list(int(input()) for _ in range(n))
-    maxVote = max(candidate)
-
-    if candidate.count(maxVote) > 1:
-        print('no winner')
+k = int(input())
+s = str(input())
+stack = []
+char = s[0]
+count = 0
+for i in range(k):
+    if s[i] == char:
+        count += 1
     else:
-        winner = candidate.index(maxVote) + 1
-        if maxVote > sum(candidate) / 2:
-            print(f'majority winner {winner}')
-        else:
-            print(f'minority winner {winner}')
+        stack.append((char, count))
+        char = s[i]
+        count = 1
+stack.append((char, count))
+
+max_length = 0
+
+for i in range(len(stack) - 1):
+    char1, len1 = stack[i]
+    char2, len2 = stack[i+1]
+
+    if char1 != char2:
+        max_length = max(max_length, min(len1, len2) * 2)
+
+print(max_length)
