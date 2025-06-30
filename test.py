f0cb57e8a6ec17f81993cc90de14978c5d60a346
7ee5u61n <le2471@gmail.com>
2025-06-30 17:41:26 +0900

    비트코인 반감기 해결

--- test.py
+++ test.py
@@ -1,24 +1,11 @@
-k = int(input())
-s = str(input())
-stack = []
-char = s[0]
-count = 0
-for i in range(k):
-    if s[i] == char:
-        count += 1
-    else:
-        stack.append((char, count))
-        char = s[i]
-        count = 1
-stack.append((char, count))
+import sys
+input = sys.stdin.readline
 
-max_length = 0
+r, k, m = map(int, input().split())
 
-for i in range(len(stack) - 1):
-    char1, len1 = stack[i]
-    char2, len2 = stack[i+1]
-
-    if char1 != char2:
-        max_length = max(max_length, min(len1, len2) * 2)
-
-print(max_length)
+for _ in range(m//k):
+    r //= 2
+    if r < 1:
+        break
+    
+print(r)
