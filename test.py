9901c1080932d88bccf51c7bbe4c854d2cbe1640
7ee5u61n <le2471@gmail.com>
2025-07-02 15:08:56 +0900

    시그마 해결

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-n, l = map(int, input().split())
-street = [0] * (l+1)
-for _ in range(n):
-    d, r, g = map(int, input().split())
-    street[d] = [r, g]
-# print(street)
-time = 0
-distance = 0
-while distance < l:
-    if street[distance]:
-        r, g = street[distance]
-        if time % (r+g) >= r:
-            distance += 1
-    else:
-        distance += 1
+import sys
+input = sys.stdin.readline
 
-    time += 1
+a, b = map(int, input().split())
 
-print(time)
+s = (a+b)*(abs(b-a)+1)//2
+
+print(s)
