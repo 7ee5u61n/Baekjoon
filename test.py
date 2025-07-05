4094e410f00e8197b81388a3fa43be8fccaeaa38
7ee5u61n <le2471@gmail.com>
2025-07-05 14:35:20 +0900

    지금 밥이 문제냐 해결

--- test.py
+++ test.py
@@ -1,16 +1,32 @@
-from collections import deque
+def iptoint(n):
+    n = n.split('.')
+    value = ''
+    for i in range(8):
+        x = str(bin(int(n[i])))[2:]
+        while len(x) < 8:
+            x = '0' + x
+        value += x
+    result = int(value, 2)
+    
+    return result
+
+def inttoip(n):
+    n = bin(int(n))[2:]
+    while len(n) < 64:
+        n = '0' + n
+    value = ''
+    for i in range(8):
+        value += str(int(n[i*8:(i+1)*8], 2))
+        if i < 7:
+            value += '.'
+    
+    return value
 
 n = int(input())
-a = deque(map(int, input().split()))
 
-count = 1
-while len(a) > 1:
-    x = a.popleft()
-    if len(a) > 0:
-        if (x+a[0])%2:
-            count += 1
-        else:
-            a.popleft()
-            a.appendleft(x)
-
-print(count)
+for i in range(n):
+    m, n = map(str, input().split())
+    if m == '1':
+        print(iptoint(n))
+    elif m == '2':
+        print(inttoip(n))
