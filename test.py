5455519965b3108849f67898eb8f0f4bcfa5d771
7ee5u61n <le2471@gmail.com>
2025-07-06 17:30:21 +0900

    얼마? 해결

--- test.py
+++ test.py
@@ -1,32 +1,9 @@
-def iptoint(n):
-    n = n.split('.')
-    value = ''
-    for i in range(8):
-        x = str(bin(int(n[i])))[2:]
-        while len(x) < 8:
-            x = '0' + x
-        value += x
-    result = int(value, 2)
-    
-    return result
+T = int(input())
+for _ in range(T):
+    s = int(input())
+    n = int(input())
+    for _ in range(n):
+        q, p = map(int, input().split())
+        s += q*p
 
-def inttoip(n):
-    n = bin(int(n))[2:]
-    while len(n) < 64:
-        n = '0' + n
-    value = ''
-    for i in range(8):
-        value += str(int(n[i*8:(i+1)*8], 2))
-        if i < 7:
-            value += '.'
-    
-    return value
-
-n = int(input())
-
-for i in range(n):
-    m, n = map(str, input().split())
-    if m == '1':
-        print(iptoint(n))
-    elif m == '2':
-        print(inttoip(n))
+    print(s)
