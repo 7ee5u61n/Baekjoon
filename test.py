9d3dce81b37f831ad9ad7559ffd11922d16def33
7ee5u61n <le2471@gmail.com>
2025-07-07 18:13:09 +0900

    나무 블록 해결

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-T = int(input())
-for _ in range(T):
-    s = int(input())
-    n = int(input())
-    for _ in range(n):
-        q, p = map(int, input().split())
-        s += q*p
+T = 1
+block = [[], ['4', '5'], [], ['4', '5'], ['2', '3'], 
+         ['8'], ['2', '3'], ['8'], ['6', '7']]
+while True:
+    n = str(input())
+    if n == '0':
+        break
 
-    print(s)
+    valid = True
+    if n[0] == '1' and n[-1] == '2':
+        for i in range(len(n)-1):
+            if n[i+1] not in block[int(n[i])]:
+                valid = False
+                break
+    else:
+        valid = False
+
+    if valid:
+        print(f'{T}. VALID')
+    else:
+        print(f'{T}. NOT')
+    T += 1
