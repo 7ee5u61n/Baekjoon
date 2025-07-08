6f7e92a0d139e68e4562acf589759306321e7e97
7ee5u61n <le2471@gmail.com>
2025-07-08 14:45:48 +0900

    팰린드롬 애너그램 해결

--- test.py
+++ test.py
@@ -1,22 +1,21 @@
-T = 1
-block = [[], ['4', '5'], [], ['4', '5'], ['2', '3'], 
-         ['8'], ['2', '3'], ['8'], ['6', '7']]
-while True:
-    n = str(input())
-    if n == '0':
+n = int(input())
+s = str(input())
+
+alphabet = [0]*26
+for i in range(n):
+    if n % 2:
+        if i == n//2:
+            continue
+    index = ord(s[i])-97
+    alphabet[index] += 1
+
+palidrome = True
+for i in alphabet:
+    if i % 2:
+        palidrome = False
         break
 
-    valid = True
-    if n[0] == '1' and n[-1] == '2':
-        for i in range(len(n)-1):
-            if n[i+1] not in block[int(n[i])]:
-                valid = False
-                break
-    else:
-        valid = False
-
-    if valid:
-        print(f'{T}. VALID')
-    else:
-        print(f'{T}. NOT')
-    T += 1
+if palidrome:
+    print('Yes')
+else:
+    print('No')
