76b474f935d9d3dd9081d44cace707ffb0b5a9a1
7ee5u61n <le2471@gmail.com>
2025-07-12 09:48:37 +0900

    수들의 합 해결

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-p = list(float(input()) for _ in range(10))
-p.sort(reverse=True)
-result = 1
-for i in range(1, 10):
-    result *= p[i-1]/i
+s = int(input())
 
-result *= int(1e9)
-print(round(result, 6))
+sm = 0
+n = 0
+
+while True:
+    n += 1
+    sm += n
+    if sm > s:
+        n -= 1
+        break
+
+print(n)
