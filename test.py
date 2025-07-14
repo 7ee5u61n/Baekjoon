42f1c21e3f9be56ad01ec8523eb773e543d475ea
7ee5u61n <le2471@gmail.com>
2025-07-14 12:36:35 +0900

    스도쿠 채점 해결

--- test.py
+++ test.py
@@ -1,23 +1,50 @@
-import sys
-input = sys.stdin.readline
+T = int(input())
+for test_case in range(1, T+1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+    
+    correct = True
+    # 행
+    for i in range(9):
+        # print(sudoku[i], i)
+        s = set()
+        for j in range(9):
+            s.add(sudoku[i][j])
+        
+        if len(s) != 9:
+            correct = False
+            break
+    # print(correct, '행')
+    
+    # 열
+    if correct:
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                s.add(sudoku[j][i])
+            
+            if len(s) != 9:
+                correct = False
+                break
+    # print(correct, '열')
 
-n = int(input())
-
-student = []
-result = 100000
-count = 0
-
-for _ in range(n):
-    info = list(map(int, input().split()))
-    if info[0] == 1:
-        student.append(info[1])
+    # 3*3
+    if correct:
+        for bi in range(3):
+            for bj in range(3):
+                s = set()
+                for ai in range(3):
+                    for aj in range(3):
+                        s.add(sudoku[bi*3+ai][bj*3+aj])
+                
+                if len(s) != 9:
+                    correct = False
+                    break
+    # print(correct, '3*3')
+    
+    if correct:
+        print(f'Case {test_case}: CORRECT')
     else:
-        student.pop(0)
-
-    if len(student) > count:
-        count = len(student)
-        result = student[-1]
-    elif len(student) == count:
-        result = min(student[-1], result)
-
-print(count, result)
+        print(f'Case {test_case}: INCORRECT')
+    
+    if test_case < T:
+        input()
