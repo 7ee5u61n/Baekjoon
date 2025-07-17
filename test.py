50a8f85408b8e90f2977b9fdf9aced57d9a88383
7ee5u61n <le2471@gmail.com>
2025-07-18 08:35:49 +0900

    젓가락 게임 해결

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-n, m = map(int, input().split())
-for i in range(1, n*m+1):
-    if i % m == 0:
-        print(i)
+yt, yj = map(int, input().split())
+
+# 1 == 용태 공격, 0 == 유진 공격
+turn = 1
+while yt < 5 or yj < 5:
+    if turn:
+        yj += yt
+        turn = 0
     else:
-        print(i, end=' ')
+        yt += yj
+        turn = 1
+
+if yt > yj:
+    print("yt")
+else:
+    print("yj")
