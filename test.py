6a680492da465101909e4c6381900f39769ffc01
7ee5u61n <le2471@gmail.com>
2025-07-19 23:06:19 +0900

    수빈이와 수열 해결

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-yt, yj = map(int, input().split())
+n = int(input())
+b = list(map(int, input().split()))
 
-# 1 == 용태 공격, 0 == 유진 공격
-turn = 1
-while yt < 5 or yj < 5:
-    if turn:
-        yj += yt
-        turn = 0
-    else:
-        yt += yj
-        turn = 1
+a = [0] * n
 
-if yt > yj:
-    print("yt")
-else:
-    print("yj")
+for i in range(n):
+    a[i] = b[i]*(i+1)-sum(a[:i])
+
+print(*a)
