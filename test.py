f90991b63ba59a9c7372053695a2cd2b420f5fc9
7ee5u61n <le2471@gmail.com>
2025-07-20 16:55:58 +0900

    광선 다각형 만들기 해결

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 n = int(input())
-b = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-a = [0] * n
+result = (90*(n-1)-sum(arr))*2
 
-for i in range(n):
-    a[i] = b[i]*(i+1)-sum(a[:i])
-
-print(*a)
+print(result)
