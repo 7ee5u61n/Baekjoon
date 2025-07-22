5bbc45e73063d6e85c7b747edd951ca691a41a9f
7ee5u61n <le2471@gmail.com>
2025-07-22 15:53:19 +0900

    순열 정렬 해결

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
-n = int(input())
-a = list(map(int,input().split()))
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    prev = min(a[0], n - a[0] + 1)
+    flag = True
+    for i in range(1, n):
+        mini = min(a[i], n - a[i] + 1)
+        maxi = max(a[i], n - a[i] + 1)
+        
+        if prev <= mini:
+            prev = mini
+        elif mini < prev <= maxi:
+            prev = maxi
+        elif maxi < prev:
+            flag = False
+            break
 
-b = list(a)
-b.sort()
-
-l = 1
-op = 0
-arr = []
-while a != b:
-    if a[l-1] != l:
-        r = a.index(l)+1
-        a[l-1:a.index(l)+1] = a[l-1:a.index(l)+1][::-1]
-        arr.append([l,r])
-        op += 1
-    l += 1
-
-print(op)
-for i in arr:
-    print(*i)
+    if flag:
+        print('YES')
+    else:
+        print('NO')
+    
