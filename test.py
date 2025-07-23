eddd6ea996b50ba49422e1adb55955e75b081445
7ee5u61n <le2471@gmail.com>
2025-07-23 11:51:29 +0900

    Darius님 한타 안 함? 해결

--- test.py
+++ test.py
@@ -1,24 +1,11 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    a = list(map(int, input().split()))
-    
-    prev = min(a[0], n - a[0] + 1)
-    flag = True
-    for i in range(1, n):
-        mini = min(a[i], n - a[i] + 1)
-        maxi = max(a[i], n - a[i] + 1)
-        
-        if prev <= mini:
-            prev = mini
-        elif mini < prev <= maxi:
-            prev = maxi
-        elif maxi < prev:
-            flag = False
-            break
+k, d, a = map(int, input().split('/'))
 
-    if flag:
-        print('YES')
-    else:
-        print('NO')
-    
+flag = True
+
+if k+a < d or d == 0:
+    flag = False
+
+if flag:
+    print('gosu')
+else:
+    print('hasu')
