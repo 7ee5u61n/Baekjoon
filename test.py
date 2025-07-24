8024280431359a64dc6b1b4de7d0e2a1fdaed6e6
7ee5u61n <le2471@gmail.com>
2025-07-24 13:33:31 +0900

    숫자 맞추기 게임 해결

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-k, d, a = map(int, input().split('/'))
+test_case = 1
+while True:
+    n = int(input())
+    if n == 0:
+        break
 
-flag = True
+    n1 = 3*n
+    if n1 % 2 == 0:
+        n2 = n1 // 2
+    else:
+        n2 = (n1 + 1) // 2
+    
+    n3 = 3*n2
 
-if k+a < d or d == 0:
-    flag = False
+    n4 = n3 // 9
 
-if flag:
-    print('gosu')
-else:
-    print('hasu')
+    if n1 % 2 == 0:
+        print(f'{test_case}. even {n4}')
+    else:
+        print(f'{test_case}. odd {n4}')
+
+    test_case += 1
