00332c9d1449b0a47d466fa7d07566d343ccfdcf
7ee5u61n <le2471@gmail.com>
2025-07-25 14:00:32 +0900

    골뱅이 찍기 - 돌아간 ㅈ 해결

--- test.py
+++ test.py
@@ -1,22 +1,16 @@
-test_case = 1
-while True:
-    n = int(input())
-    if n == 0:
-        break
+n = int(input())
 
-    n1 = 3*n
-    if n1 % 2 == 0:
-        n2 = n1 // 2
-    else:
-        n2 = (n1 + 1) // 2
+for i in range(n):
+    print('@'*n+' '*3*n+'@'*n)
+
+for i in range(n):
+    print('@'*n+' '*2*n+'@'*n)
     
-    n3 = 3*n2
+for i in range(n):
+    print('@'*3*n)
 
-    n4 = n3 // 9
-
-    if n1 % 2 == 0:
-        print(f'{test_case}. even {n4}')
-    else:
-        print(f'{test_case}. odd {n4}')
-
-    test_case += 1
+for i in range(n):    
+    print('@'*n+' '*2*n+'@'*n)
+    
+for i in range(n):
+    print('@'*n+' '*3*n+'@'*n)
