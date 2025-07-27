f82049c27389a5677d9e1e4e1ec730ec4103bb06
7ee5u61n <le2471@gmail.com>
2025-07-28 08:34:50 +0900

    Cow Race 해결

--- test.py
+++ test.py
@@ -1,19 +1,72 @@
+
 n, m = map(int, input().split())
 
-a = list(input() for _ in range(n))
-b = list(input() for _ in range(n))
+bessie = []
+# 총 시간
+totalTime = 0
+for _ in range(n):
+    speed, time = map(int, input().split())
+    totalTime += time
+    bessie.append((speed, time))
 
-result = True
-for i in range(n):
-    temp = ''        
-    for j in range(m):
-        temp += a[i][j]*2
+elsie = []
+for _ in range(m):
+    speed, time = map(int, input().split())
+    elsie.append((speed, time))
 
-    if temp != b[i]:
-        result = False
-        break
+# 각자 위치
+bessiePosition = 0
+elsiePosition = 0
 
-if result:
-    print("Eyfa")
-else:
-    print("Not Eyfa")
+# 현재 선두
+currentLeader = -1
+
+# 현재 속도 인덱스
+bessieIndex = 0
+elsieIndex = 0
+
+# 달린 유닛 시간
+bessieUnitTime = 0
+elsieUnitTime = 0
+
+# 선두 변경 횟수
+count = 0
+for _ in range(totalTime):
+    bessiePosition += bessie[bessieIndex][0]
+    elsiePosition += elsie[elsieIndex][0]
+
+    # 각자 유닛 시간 증가
+    bessieUnitTime += 1
+    elsieUnitTime += 1
+
+    # 현재 속도 인덱스가 가리키는 시간만큼 달렸다면 인덱스 증가
+    if bessieUnitTime == bessie[bessieIndex][1]:
+        bessieIndex += 1
+        bessieUnitTime = 0
+
+    if elsieUnitTime == elsie[elsieIndex][1]:
+        elsieIndex += 1
+        elsieUnitTime = 0
+
+    # 선두 확인
+    if bessiePosition > elsiePosition:
+        # 베시
+        leadership = 1
+    elif elsiePosition > bessiePosition:
+        # 엘시
+        leadership = 0 
+    else:
+        # 동점
+        leadership = 2
+
+    # 선두 변경
+    if currentLeader == -1:
+        currentLeader = leadership
+    elif leadership != 2 and leadership != currentLeader:
+        if currentLeader == 2:
+            currentLeader = leadership
+        elif currentLeader != 2 and leadership != currentLeader:
+            count += 1
+            currentLeader = leadership
+
+print(count)
