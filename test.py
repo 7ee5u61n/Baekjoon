d6eb8bb29c14f9598d3dc06b8982160dabdba952
7ee5u61n <le2471@gmail.com>
2025-07-30 16:02:49 +0900

    SciComLove 해결

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-a1, a0 = map(int, input().split())
-c1, c2 = map(int, input().split())
-n0 = int(input())
-
-fx = a1 * n0 + a0
-gx = n0
-
-if c1*gx <= fx <= c2*gx:
-    print(1)
-else:
-    print(0)
+n = int(input())
+for _ in range(n):
+    print('SciComLove')
