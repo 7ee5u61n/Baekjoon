d4cf83fa26be208f670c5f15609b38fb24e1dafb
7ee5u61n <le2471@gmail.com>
2025-08-01 18:01:14 +0900

    Alea Iacta Est 해결

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-a, b, c, d = map(int, input().split())
+a, b = map(int, input().split())
+c, d = map(int, input().split())
 
-result = min(abs(a+b-(c+d)), abs(a+c-(b+d)), abs(a+d-(b+c)))
-print(result)
+gadong = 1
+for i in range(a+b-1):
+    gadong += 1
+    if gadong > 4:
+        gadong = 1
+
+jindong = int(gadong)
+for i in range(c+d-1):
+    jindong += 1
+    if jindong > 4:
+        jindong = 1
+
+print(jindong)
