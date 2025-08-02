db9264720fb2a495baf23162c0d0d68fccc68b84
7ee5u61n <le2471@gmail.com>
2025-08-02 17:59:46 +0900

    시계탑 해결

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-a, b = map(int, input().split())
-c, d = map(int, input().split())
+m = int(input())
 
-gadong = 1
-for i in range(a+b-1):
-    gadong += 1
-    if gadong > 4:
-        gadong = 1
+result = 0
+for i in range(m):
+    if i < 30:
+        result += 0.5
+    else:
+        result += 1.5
 
-jindong = int(gadong)
-for i in range(c+d-1):
-    jindong += 1
-    if jindong > 4:
-        jindong = 1
-
-print(jindong)
+print(f'{result:.1f}')
