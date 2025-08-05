7654a92dc499cc8d48923af66d5ffcfb43221dfb
7ee5u61n <le2471@gmail.com>
2025-08-05 16:58:27 +0900

    수 이어가기 해결

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 n = int(input())
-v = list(map(int, input().split()))
 
-start = v.index(max(v))+1
+result = []
+for i in range(1, n+1):
+    arr = [n, i]
+    j = 1
+    while True:
+        value = arr[j-1]-arr[j]
+        if value < 0:
+            break
+        arr.append(value)
+        j += 1
+    
+    if len(result) < len(arr):
+        result = []
+        result += arr
 
-result = 0
-for i in range(n-1):
-    result += v[(i+start)%n]
-
-print(result)
+print(len(result))
+print(*result)
