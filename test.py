77067cee1dd79f6b98b740f82df8d7211f978686
7ee5u61n <le2471@gmail.com>
2025-08-06 14:35:38 +0900

    !밀비 급일 해결

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-n = int(input())
-
-result = []
-for i in range(1, n+1):
-    arr = [n, i]
-    j = 1
-    while True:
-        value = arr[j-1]-arr[j]
-        if value < 0:
-            break
-        arr.append(value)
-        j += 1
-    
-    if len(result) < len(arr):
-        result = []
-        result += arr
-
-print(len(result))
-print(*result)
+while True:
+    code = str(input())
+    if code == 'END':
+        break
+    print(code[::-1])
