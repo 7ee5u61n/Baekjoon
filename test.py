717f992061163f916cc52ae78a83209800782bca
7ee5u61n <le2471@gmail.com>
2025-08-10 19:58:04 +0900

    교수님 그림이 깨지는데요? 해결

--- test.py
+++ test.py
@@ -1,22 +1,10 @@
-import sys
-input = sys.stdin.readline
+n, k = map(int, input().split())
 
-n = int(input())
-x = list(map(int, input().split()))
-l = list(map(int, input().split()))
-c = list(map(str, input().split()))
+arr = list(list(map(int, input().split())) for _ in range(n))
 
-result = False
 for i in range(n):
-    for j in range(i+1, n):
-        if abs(x[i]-x[j]) <= l[i]+l[j] and c[i] != c[j]:
-            result = True
-            break
-    if result:
-        break
-
-if result:
-    print("YES")
-    print(i+1, j+1)
-else:
-    print("NO")
+    temp = ''
+    for j in range(n):
+        temp += ((str(arr[i][j])+' ')*k)
+    for j in range(k):
+        print(temp)
