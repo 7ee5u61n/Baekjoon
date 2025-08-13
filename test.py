5fd01fcf9f87e5076ad9b162107e3c3be0851956
7ee5u61n <le2471@gmail.com>
2025-08-13 14:57:42 +0900

    오늘의 날짜는? 해결

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-import math
+from datetime import datetime
+now = datetime.now()
 
-def solve():
-    a, b, c = map(int, input().split())
-    
-    p = a
-    q = 2 * b * c
-    r = a * (c * c - a * a + b * b)
-
-    d = q * q - 4 * p * r
-
-    if d < 0:
-        print("-1")
-        return
-
-    x = (-q + math.sqrt(d)) / (2 * p)
-
-    if x < 0:
-        print("-1")
-        return
-
-    result = int(x)
-    print(result)
-
-solve()
+print(now.year)
+print(now.month)
+print(now.day)
