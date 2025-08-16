1757527ff973e5c507962113558d7f8075611fd0
7ee5u61n <le2471@gmail.com>
2025-08-16 12:38:17 +0900

    소수가 아닌 수 해결

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+def is_prime(n):
+    end = int(n**(0.5))
+    for i in range(2, end+1):
+        if n % i == 0:
+            return False
+    return True
+
 n = int(input())
 
-for i in range(1, n+1):
-    s = input()
-
-    print(f'{i}. {s}')
+for i in range(n, int(1e9)+1):
+    if is_prime(i) == False:
+        print(i)
+        break
