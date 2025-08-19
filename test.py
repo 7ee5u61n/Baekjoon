3989d11562db9505b3a8c16529255afae972568b
7ee5u61n <le2471@gmail.com>
2025-08-19 16:38:51 +0900

    소수가 아닌 수 3 해결

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
 
-n, q = map(int, input().split())
-arr = [0]*(n+1)
-
-for _ in range(q):
-    l, p, x = map(int, input().split())
-
-    if l == 1:
-        arr[p] += x
-    elif l == 2:
-        print(sum(arr[p:x+1]))
-    
+print('YES')
+print(arr[0]*111)
