2ee2086aa6a105ed0d3f0ab4a5a6a270880cb584
7ee5u61n <le2471@gmail.com>
2025-08-27 12:06:35 +0900

    고려대는 사랑입니다 해결

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-n = int(input())
+import sys
 
-mn = int(10e6)*6
-result = []
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i * j > n:
-            break
-        for k in range(1, n+1):
-            if i * j * k > n:
-                break
-            elif i*j*k == n:
-                if 2*(i*j + j*k + k*i) < mn:
-                    mn = 2*(i*j + j*k + k*i)
-                    result = [i, j, k]
-                
-print(*result)
+sys.stdout.write("고려대학교\n")
