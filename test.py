35423e1dec84a59b3db5164c6392347630a16195
7ee5u61n <le2471@gmail.com>
2025-08-29 16:58:55 +0900

    피갤컵  해결

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
+year = 2024
+month = 8
+
 n = int(input())
-print(n-2024)
+
+
+month += 7*(n-1)
+
+if month > 12:
+    year += month // 12
+    month = month % 12
+    if month == 0:
+        month = 12
+        year -= 1
+
+print(year, month)
