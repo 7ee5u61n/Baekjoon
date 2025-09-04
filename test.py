7a6dc52f0c99034591df27da6227bcb51a8c0674
7ee5u61n <le2471@gmail.com>
2025-09-04 22:43:21 +0900

    콜센터 해결

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-s = str(input())
-
-if s[0] == '"' and s[-1] == '"':
-    if s[1:-1]:
-        print(s[1:-1])
-    else:
-        print('CE')
-else:
-    print('CE')
+print('     /~\\')
+print('    ( oo|')
+print('    _\\=/_')
+print('   /  _  \\')
+print('  //|/.\|\\\\')
+print(' ||  \\ /  ||')
+print('============')
+print('|          |')
+print('|          |')
+print('|          |')
