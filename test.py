0fe3a42e931bea4a5f7688f03b73ed91c1e60b75
7ee5u61n <le2471@gmail.com>
2025-09-08 17:04:15 +0900

    바코드 닉네임 해결

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-x0, n = map(int, input().split())
-x = [x0]
+n = int(input())
+s = str(input())
 
-for i in range(n+1):
-    if x[i] % 2 == 0:
-        x.append((x[i]//2)^6)
+for i in range(n):
+    if s[i].isupper():
+        print(s[i].lower(), end='')
     else:
-        x.append((2*x[i])^6)
+        print(s[i].upper(), end='')
 
-print(x[n])
