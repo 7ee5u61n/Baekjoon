72fc76819dc7e9aa911602eecc89bcff372c3d44
7ee5u61n <le2471@gmail.com>
2025-09-13 17:40:31 +0900

    나이 계산하기 해결

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-n = int(input())
+year, month, day = map(int, input().split())
+y, m, d = map(int, input().split())
 
-for _ in range(n):
-    k = int(input())
-    print('='*k)
+manAge = 0
+seneunAge = 0
+yeonAge = 0
+
+manAge = y-year
+if month > m or (month == m and day > d):
+    manAge -= 1
+
+seneunAge = y-year+1
+
+if year == y:
+    yeonAge = 0
+else:
+    yeonAge = y-year
+
+print(manAge)
+print(seneunAge)
+print(yeonAge)
