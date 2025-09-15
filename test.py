c2e0f1875aa8ef5e0a68aa1471f4f82739f971fb
7ee5u61n <le2471@gmail.com>
2025-09-15 19:11:51 +0900

    인간은 무엇인가 해결

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
 n = int(input())
-k = str(input())
 
-odd = 0
-even = 0
-for i in range(n):
-    if int(k[i]) % 2 == 0:
-        even += 1
-    else:
-        odd += 1
-
-if even > odd:
-    print(0)
-elif odd > even:
-    print(1)
+if n % 2024 == 0 and n <= 100000:
+    print('Yes')
 else:
-    print(-1)
+    print('No')
