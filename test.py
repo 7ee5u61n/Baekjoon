58943a079c5be71a3d12a6787374edc6f22c9726
7ee5u61n <le2471@gmail.com>
2025-09-27 15:32:40 +0900

    모범생 포닉스 해결

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-k, n, m = map(int, input().split())
+n = int(input())
+plan = list(map(int, input().split()))
 
-if k*n > m:
-    print(k*n-m)
-else:
-    print(0)
+time = 0
+for i in range(n):
+    time += plan[i]
+    if i < len(plan)-1:
+        time += 8
+
+d = time // 24
+h = time % 24
+
+print(d)
+print(h)
