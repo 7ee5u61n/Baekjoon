ca17f0deb1fdc531d1a42c04ccb2e47ba6255da6
7ee5u61n <le2471@gmail.com>
2025-09-28 18:54:48 +0900

    양파 실험 해결

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
-n = int(input())
-plan = list(map(int, input().split()))
+n, a, b = map(int, input().split())
 
-time = 0
+o1, o2 = 1, 1
+
+switch = True
 for i in range(n):
-    time += plan[i]
-    if i < len(plan)-1:
-        time += 8
+    if switch:
+        o1 += a
+        o2 += b
+        
+        if o1 < o2:
+            switch = False
+        elif o1 == o2:
+            o2 -= 1
 
-d = time // 24
-h = time % 24
+    else:
+        o1 += b
+        o2 += a
 
-print(d)
-print(h)
+        if o2 < o1:
+            switch = True
+        elif o1 == o2:
+            o1 -= 1
+
+if switch:
+    print(o1, o2)
+else:
+    print(o2, o1)
