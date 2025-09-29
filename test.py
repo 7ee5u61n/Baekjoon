01f7387d6c44de6318ea9be98eeb4d994c6556ae
7ee5u61n <le2471@gmail.com>
2025-09-29 18:18:35 +0900

    네 수 해결

--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-n, a, b = map(int, input().split())
+a, b, c, d = map(str, input().split())
 
-o1, o2 = 1, 1
-
-switch = True
-for i in range(n):
-    if switch:
-        o1 += a
-        o2 += b
-        
-        if o1 < o2:
-            switch = False
-        elif o1 == o2:
-            o2 -= 1
-
-    else:
-        o1 += b
-        o2 += a
-
-        if o2 < o1:
-            switch = True
-        elif o1 == o2:
-            o1 -= 1
-
-if switch:
-    print(o1, o2)
-else:
-    print(o2, o1)
+print(int(a+b)+int(c+d))
