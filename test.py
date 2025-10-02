ad5b837773ef24e5f40276e45641bd15d151b176
7ee5u61n <le2471@gmail.com>
2025-10-02 11:02:38 +0900

    Goodbye, Code Jam 해결

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-x = int(input())
+T = int(input())
+for test_case in range(1, T+1):
+    n = int(input())
 
-if x % 3 == 1:
-    print('U')
-elif x % 3 == 2:
-    print('O')
-else:
-    print('S')
+    if n > 4500:
+        print(f"Case #{test_case}: Round 1")
+    elif n > 1000:
+        print(f"Case #{test_case}: Round 2")
+    elif n > 25:
+        print(f"Case #{test_case}: Round 3")
+    else:
+        print(f"Case #{test_case}: World Finals")
