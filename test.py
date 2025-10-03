e7b4846fd2bf84341eb907a777bb7bb5b1f32ee0
7ee5u61n <le2471@gmail.com>
2025-10-03 09:02:14 +0900

    DORO 해결

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-T = int(input())
-for test_case in range(1, T+1):
-    n = int(input())
+n = int(input())
+arr = list(map(str, input().split()))
 
-    if n > 4500:
-        print(f"Case #{test_case}: Round 1")
-    elif n > 1000:
-        print(f"Case #{test_case}: Round 2")
-    elif n > 25:
-        print(f"Case #{test_case}: Round 3")
-    else:
-        print(f"Case #{test_case}: World Finals")
+for i in range(n):
+    print(arr[i] + 'DORO', end=' ')
