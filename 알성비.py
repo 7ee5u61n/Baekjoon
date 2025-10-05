88e18aefba5dee9c224b59b60f13ce689d1a9634
7ee5u61n <le2471@gmail.com>
2025-10-05 10:43:24 +0900

    복구

--- 알성비.py
+++ 알성비.py
@@ -1,13 +1,10 @@
-n = int(input())
-
-count = 1
+print('가격: 원, 용량: ml, 도수: %. 순으로 입력')
+price = capacity = volume = 1
 while True:
-    if n == 1:
-        break    
-    if n % 2 == 0:
-        n //= 2
+    price, capacity, volume = map(int, input().split())
+    if price and capacity and volume:
+        result = price / (capacity * volume)
+        print(f'가격: {price}원, 용량: {capacity}ml, 도수: {volume}%, 가격/알코올: {result:.2f}원/ml')
     else:
-        n = n * 3 + 1
-    count += 1
-
-print(count)
+        print('종료합니다.')
+        break
