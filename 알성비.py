e67fa7e1aa6b7529426a752f188d603b703e678b
7ee5u61n <le2471@gmail.com>
2025-10-14 10:39:39 +0900

    사과와 바나나 나눠주기 해결

--- 알성비.py
+++ 알성비.py
@@ -1,10 +1,10 @@
-print('가격: 원, 용량: ml, 도수: %. 순으로 입력')
-price = capacity = volume = 1
-while True:
-    price, capacity, volume = map(int, input().split())
-    if price and capacity and volume:
-        result = price / (capacity * volume)
-        print(f'가격: {price}원, 용량: {capacity}ml, 도수: {volume}%, 가격/알코올: {result:.2f}원/ml')
-    else:
-        print('종료합니다.')
-        break
+a, b = map(int, input().split())
+
+result = []
+
+for i in range(1, a+b+1):
+    if a % i == 0 and b % i == 0:
+        result.append([i, a // i, b // i])
+
+for i in result:
+    print(*i)
