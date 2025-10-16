a7cbced88b2fe813675d28f13d9b96e9efd749c3
7ee5u61n <le2471@gmail.com>
2025-10-16 10:29:07 +0900

    수학은 체육과목 입니다 2 해결

--- 알성비.py
+++ 알성비.py
@@ -1,6 +1,13 @@
-t, s = map(int, input().split())
+n = int(input())
 
-if 12 <= t <= 16 and s == 0:
-    print(320)
-else:
-    print(280)
+if n % 16 == 1 or n % 16 == 9:
+    print(1)
+elif n % 16 == 2 or n % 16 == 8 or n % 16 == 10 or n % 16 == 0:
+    print(2)
+elif n % 16 == 3 or n % 16 == 7 or n % 16 == 11 or n % 16 == 15:
+    print(3)
+elif n % 16 == 4 or n % 16 == 6 or n % 16 == 12 or n % 16 == 14:
+    print(4)
+elif n % 16 == 5 or n % 16 == 13:
+    print(5)
+
