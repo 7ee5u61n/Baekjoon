ae14fe040080d630df36b3faa182cbdcd35d1314
7ee5u61n <le2471@gmail.com>
2025-10-17 09:14:24 +0900

    관공... 어찌하여 목만 오셨소... 해결

--- 알성비.py
+++ 알성비.py
@@ -1,13 +1,10 @@
 n = int(input())
 
-if n % 16 == 1 or n % 16 == 9:
-    print(1)
-elif n % 16 == 2 or n % 16 == 8 or n % 16 == 10 or n % 16 == 0:
-    print(2)
-elif n % 16 == 3 or n % 16 == 7 or n % 16 == 11 or n % 16 == 15:
-    print(3)
-elif n % 16 == 4 or n % 16 == 6 or n % 16 == 12 or n % 16 == 14:
-    print(4)
-elif n % 16 == 5 or n % 16 == 13:
-    print(5)
+result = ''
+for _ in range(n):
+    name = str(input())
+    
+    if 'S' in name:
+        result = name
 
+print(result)
