536c8e85e0ccc9692900e45ac6ca7e2904df1af1
7ee5u61n <le2471@gmail.com>
2025-10-20 11:37:36 +0900

    복구

--- 알성비.py
+++ 알성비.py
@@ -1,12 +1,10 @@
-n = int(input())
-
-high = 0
-for _ in range(n):
-    a, d, g = map(int, input().split())
-    point = a*(d+g)
-    if a == d+g:
-        point *= 2
-
-    high = max(high, point)
-
-print(high)
+print('가격: 원, 용량: ml, 도수: %. 순으로 입력')
+price = capacity = volume = 1
+while True:
+    price, capacity, volume = map(int, input().split())
+    if price and capacity and volume:
+        result = price / (capacity * volume)
+        print(f'가격: {price}원, 용량: {capacity}ml, 도수: {volume}%, 가격/알코올: {result:.2f}원/ml')
+    else:
+        print('종료합니다.')
+        break
