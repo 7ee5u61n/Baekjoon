ec60671cac8208fdc879b0d96141ff4af9b47054
7ee5u61n <le2471@gmail.com>
2025-10-22 11:12:09 +0900

    정보섬의 대중교통 해결

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-T = int(input())
+n, a, b = map(int, input().split())
 
-for _ in range(T):
-    arr = list(map(int, input().split()))
-    h = max(arr[0]+arr[4], 1)
-    m = max(arr[1]+arr[5], 1)
-    a = max(arr[2]+arr[6], 0)
-    b = arr[3]+arr[7]
-    power = h+5*m+2*a+2*b
-    print(power)
+if a < b:
+    print('Bus')
+elif a > b:
+    print('Subway')
+else:
+    print('Anything')
