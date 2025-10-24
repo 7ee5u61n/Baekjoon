aa81708d255c503c4581116e6ca9377c1a90a11c
7ee5u61n <le2471@gmail.com>
2025-10-24 16:53:59 +0900

    찾아오시는 길 해결

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-t = 0
-for _ in range(4):
-    ti =  int(input())
-    t += ti
-    
-if t <= 1500:
-    print('Yes')
-else:
-    print('No')
+a, b = map(int, input().split())
+c, d = map(int, input().split())
 
+if a + c < b + d:
+    print("Hanyang Univ.")
+elif a + c > b + d:
+    print("Yongdap")
+else:
+    print("Either")
