f519a6435dcbe5660576eb13c94bde82e51ced72
7ee5u61n <le2471@gmail.com>
2025-11-05 16:40:23 +0900

    연세대학교 해결

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
-s = str(input())
-k = str(input())
+n = int(input())
 
-temp = ''
-for i in s:
-    if i.isalpha():
-        temp += i
-
-result = False
-for i in range(len(temp)-len(k)+1):
-    if temp[i:i+len(k)] == k:
-        result = True
-        break
-        
-if result:
-    print(1)
+if n:
+    print('Leading the Way to the Future')
 else:
-    print(0)
+    print('YONSEI')
