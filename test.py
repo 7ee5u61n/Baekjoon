14f92ae24fbcf1f5bbfb351c42d6d7b2d70e1b92
7ee5u61n <le2471@gmail.com>
2025-11-06 16:04:16 +0900

    세제곱의 합 해결

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 n = int(input())
 
-if n:
-    print('Leading the Way to the Future')
-else:
-    print('YONSEI')
+a = 0
+b = 0
+c = 0
+for i in range(1, n+1):
+    a += i
+    c += i**3
+
+b = a**2
+
+print(a)
+print(b)
+print(c)
