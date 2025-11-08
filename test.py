d1bb90a1787efbfe649b694605f818151ea75adc
7ee5u61n <le2471@gmail.com>
2025-11-08 15:26:57 +0900

    달달함이 넘쳐흘러 해결

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-n, m = map(int, input().split())
+ax, ay, az = map(int, input().split())
+cx, cy, cz = map(int, input().split())
 
-for i in range(n):
-    row = str(input())
-    print(row[::-1])
+bx = cx - az
+by = cy // ay
+bz = cz - ax
+
+print(bx, by, bz)
