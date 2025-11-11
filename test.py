c820bd296c3e8d9bf9eb2ef73b08d88cc543231d
7ee5u61n <le2471@gmail.com>
2025-11-11 16:04:47 +0900

    K 2K 게임 해결

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-arr = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-print(sum(arr))
+n, k = map(int, input().split())
+
+fk = k % 10
+fk2 = (k*2) % 10
+
+arr = []
+for i in range(1, n + 1):
+    if i % 10 != fk and i % 10 != fk2:
+        arr.append(i)
+
+print(len(arr))
+print(*arr)
