23060dbf51ca21b48b20fc1a78a753fa4592b11c
7ee5u61n <le2471@gmail.com>
2025-11-12 13:14:57 +0900

    웰컴 해결

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import sys
-input = sys.stdin.readline
-
-n, k = map(int, input().split())
-
-fk = k % 10
-fk2 = (k*2) % 10
-
-arr = []
-for i in range(1, n + 1):
-    if i % 10 != fk and i % 10 != fk2:
-        arr.append(i)
-
-print(len(arr))
-print(*arr)
+print('.  .   .')
+print('|  | _ | _. _ ._ _  _')
+print('|/\\|(/.|(_.(_)[ | )(/.')
