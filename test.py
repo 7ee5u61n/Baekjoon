932dac97ea85f16e8eeb3616fd82090e09c45758
7ee5u61n <le2471@gmail.com>
2025-11-15 15:32:56 +0900

    AFC 윔블던 해결

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-n = int(input())
+n, m = map(int, input().split())
 
-for _ in range(n):
-   print('I love DGU')
+result = []
+for i in range(1000):
+    for j in range(1000):
+        if i + j == n and i - j == m:
+            result = [i, j]
+            break
+result = sorted(result, reverse=True)
+if result:
+    print(*result)
+else:
+    print(-1)
