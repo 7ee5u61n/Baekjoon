6cd6c0f548613a023d2916b76aeca3b27c00a7ca
7ee5u61n <le2471@gmail.com>
2025-12-11 15:52:03 +0900

    지폐 세기 해결

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-a, b = map(int, input().split())
+n = int(input())
 
-print(min((a//2), b))
+result = 0
+
+for i in range(n):
+    a, b = map(int, input().split())
+    if a == 136:
+        result += 1000
+    elif a == 142:
+        result += 5000
+    elif a == 148:
+        result += 10000
+    elif a == 154:
+        result += 50000
+
+print(result)
