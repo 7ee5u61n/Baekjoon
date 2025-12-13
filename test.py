dd0b1db3dda61a12bdde7ea4bdce983c14990aad
7ee5u61n <le2471@gmail.com>
2025-12-13 12:48:28 +0900

    현대모비스와 함께하는 부품 관리 해결

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('nkugwan')
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    n = int(input())
+    a, b = map(int, input().split())
+    for _ in range(n):
+        ui, vi = map(int, input().split())
+    
+    print(f'Material Management {test_case}')
+    print('Classification ---- End!')
