010d61dca86dabcd718d3072badc6bb518afe8db
7ee5u61n <le2471@gmail.com>
2025-12-19 21:05:25 +0900

    엘리스 트랙 매칭 해결

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-promise = {'Never gonna give you up', 'Never gonna let you down', 'Never gonna run around and desert you', 'Never gonna make you cry', 
-           'Never gonna say goodbye', 'Never gonna tell a lie and hurt you', 'Never gonna stop'}
-
 n = int(input())
-result = False
-for _ in range(n):
-    line = input()
-    if line not in promise:
-        result = True
+friend = list(input())
+hellobit = str(input())
 
-if result:
-    print("Yes")
-else:
-    print("No")
+print(friend.count(hellobit))
