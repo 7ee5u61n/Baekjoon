736b9864a87674105d9634eec10439a91248d389
7ee5u61n <le2471@gmail.com>
2025-12-20 22:20:57 +0900

    2017 연세대학교 프로그래밍 경시대회 해결

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+result = 0
 n = int(input())
-friend = list(input())
-hellobit = str(input())
 
-print(friend.count(hellobit))
+# 택희, 영훈, 남규
+temp = 0
+for i in range(2, n+1, 2):
+    temp += i
+    for j in range(1, n-i+1):
+        temp += j
+        for k in range(j+2, n-i-j+2):
+            temp += k
+            if temp == n:
+                result += 1
+            temp -= k
+        temp -= j
+    temp -= i
+
+print(result)
