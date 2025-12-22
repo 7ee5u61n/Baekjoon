331ace60946d6a19831aef000b5a3032590c93df
7ee5u61n <le2471@gmail.com>
2025-12-22 20:58:01 +0900

    꿍의 우주여행 해결

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-a, b, c, d = map(int, input().split())
-p, m, n = map(int, input().split())
-
-dog = [0]*1000
-for i in range(1000):
-    if 0 < i % (a+b) <= a:
-        dog[i] += 1
-    if 0 < i % (c+d) <= c:
-        dog[i] += 1
-
-print(dog[p])
-print(dog[m])
-print(dog[n])
+T = int(input())
+for _ in range(T):
+    n, d = map(int, input().split())
+    result = 0
+    for _ in range(n):
+        v, f, c = map(int, input().split())
+        if v * (f/c) >= d:
+            result += 1
+    print(result)
