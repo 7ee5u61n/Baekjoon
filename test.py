874ff92d84e1d7313570b67764f3f147bc2beda3
7ee5u61n <le2471@gmail.com>
2025-12-25 15:49:46 +0900

    인공지능 시계 해결

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-a, t = map(int, input().split())
-p = 10+2*(25-a+t)
-print(max(p, 0))
+a, b, c = map(int, input().split())
+d = int(input())
+
+total_seconds = a * 3600 + b * 60 + c + d
+new_hour = (total_seconds // 3600) % 24
+new_minute = (total_seconds % 3600) // 60
+new_second = total_seconds % 60
+
+print(new_hour, new_minute, new_second)
