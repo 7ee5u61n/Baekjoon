bc3a221a724cf4fc4c3fc5665153c701d7df335d
7ee5u61n <le2471@gmail.com>
2025-12-31 14:43:52 +0900

    채플이 너무해 해결

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-while True:
-    try:
-        m, p, l, e, r, s, n = map(int, input().split())
-    except:
-        break
-    for i in range(n):
-        mp = m
-        m = p//s
-        p = l//r
-        l = mp*e
-
-    print(m)
+x = int(input())
+if x >= 6:
+    print('Success!')
+else:
+    print('Oh My God!')
