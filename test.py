14a315e2e09aaa41be9d76280c45917792b6db24
7ee5u61n <le2471@gmail.com>
2026-01-05 15:33:49 +0900

    에어팟 해결

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-n = str(input())
+n = int(input())
+arr = list(map(int, input().split()))
 
-result = 0
-while len(n) > 1:
-    sum_digits = 1
-    for digit in n:
-        sum_digits *= int(digit)
-    result += 1
-    n = str(sum_digits)
-
-print(result)
+connected = 0
+battery = 0
+previous_battery = 2
+for i in range(n):
+    # 이미 연결되어 있는 핸드폰에 연결
+    if connected == arr[i]:
+        # 직전 배터리 소모량의 2배
+        previous_battery *= 2
+        battery += previous_battery
+    # 새로운 핸드폰 연결
+    else:
+        connected = arr[i]
+        battery += 2
+        previous_battery = 2
+    if battery >= 100:
+        connected = 0
+        battery = 0
+print(battery)
