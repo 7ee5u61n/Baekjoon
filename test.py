390b9ca0846cad50632f74256a67e1a2b56e03b1
7ee5u61n <le2471@gmail.com>
2026-01-12 14:06:17 +0900

    이진수 해결

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-n = int(input())
-
-weather = list(map(int, input().split()))
-
-result = [0]
-for i in weather:
-    if i:
-        result.append(result[-1]+1)
-    else:
-        result.append(result[-1]-1)
-
-print(sum(result))
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    n = bin(n)[2:]
+    for i in range(len(n)-1, -1, -1):
+        if n[i] == '1':
+            print(len(n)-i-1, end=' ')
+    print()
