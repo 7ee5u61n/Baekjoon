2119d0dac68359548794af2f3c75c962a7d8bec8
7ee5u61n <le2471@gmail.com>
2026-01-16 14:04:27 +0900

    가희와 클럽 오디션 1 해결

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-n, m, k = map(int, input().split())
+lv, judge = input().split()
 
-print(k//m, k%m)
+if judge == 'miss':
+    print(0)
+elif judge == 'bad':
+    print(int(lv) * 200)
+elif judge == 'cool':
+    print(int(lv) * 400)
+elif judge == 'great':
+    print(int(lv) * 600)
+elif judge == 'perfect':
+    print(int(lv) * 1000)
