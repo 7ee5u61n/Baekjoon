1674485be7859526171daa00b052a420f7ad993c
7ee5u61n <le2471@gmail.com>
2026-01-29 13:46:43 +0900

    한글 해결

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-n, m = map(int, input().split())
-
-room = [[0, 0] for _ in range(n + 1)]
-
-for i in range(m):
-    k, s, e = map(int, input().split())
-    if room[k][1] > s:
-        print('NO')
-        continue
-    room[k][0] = s
-    room[k][1] = e
-    print('YES')
+print(chr(n+ord('가')-1))
