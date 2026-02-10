45726ea3c1ab2c9cb44f272a3ba65074d7b59a0c
7ee5u61n <le2471@gmail.com>
2026-02-10 22:39:05 +0900

    e 계산 해결

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-n, m, k = map(int, input().split())
-
-mn = max(n-(m*k), 0)
-mx = n-m*(k-1)-1
-
-print(mn, mx)
+def factorial(n):
+    sum = 1
+    for i in range(1, n+1):
+        sum *= i
+    if n == 0:
+        return 1
+    else:
+        return sum
+    
+print('n e')
+print('- -----------')
+sum = 0
+for i in range(10):
+    sum += 1 / factorial(i)
+    if i < 2:
+        print(i, int(sum))
+    elif i == 2:
+        print(i, float(sum))
+    else:
+        print(i, '{0:.9f}'.format(sum))
