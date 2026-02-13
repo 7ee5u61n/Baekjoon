8b2e488a017aede02d112a68a4ada66558a516cf
7ee5u61n <le2471@gmail.com>
2026-02-13 11:35:22 +0900

    할인이 필요해 해결

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    arr = list(map(int, input().split()))
-    
-    print(sum(arr))
+n, m, s = map(int, input().split())
+
+a = int(s*(m+1)*(100-n)/100)
+b = s*m
+
+print(min(a, b))
