33f220e2d8b409edfebdaeedaa10e17765dfe2bd
7ee5u61n <le2471@gmail.com>
2026-02-18 15:42:01 +0900

    한글 2

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-n = int(input())
-point = []
-for i in range(1, n+1):
-    s, c, l = map(int, input().split())
-    point.append((s, c, l, i))
-
-point.sort(key=lambda x: (-x[0], x[1], x[2]))
-
-print(point[0][3])
+s = str(input())
+print(ord(s)-44031)
