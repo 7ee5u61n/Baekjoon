2755ed4cb02edb70f4ea2e7fc55dc6a88465c564
7ee5u61n <le2471@gmail.com>
2026-02-20 11:43:30 +0900

    체스 초보 브실이 해결

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-n = int(input())
+chess = {'K': 0, 'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9,
+         'k': 0, 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, '.': 0}
 
-sm = 1/(7*24*60*60)
-for i in range(1, n+1):
-    sm*= i
+white = 0
+black = 0
+for i in range(8):
+    row = str(input())
+    for piece in row:
+        if piece.isupper():
+            white += chess[piece]
+        else:
+            black += chess[piece]
 
-print(int(sm))
+print(white - black)
