d8add5c2de40fa229f76ccc0989bee432035c07f
7ee5u61n <le2471@gmail.com>
2026-02-21 15:37:49 +0900

    코드마스터 2024 해결

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-chess = {'K': 0, 'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9,
-         'k': 0, 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, '.': 0}
+n, m, k = map(int, input().split())
 
-white = 0
-black = 0
-for i in range(8):
-    row = str(input())
-    for piece in row:
-        if piece.isupper():
-            white += chess[piece]
-        else:
-            black += chess[piece]
+dohun = k*m
+hyeongjun = m
 
-print(white - black)
+print(dohun + hyeongjun)
