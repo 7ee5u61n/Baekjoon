a98aefba161615afbfeabb1f9b96d9835f8c00ae
7ee5u61n <le2471@gmail.com>
2026-02-22 15:24:23 +0900

    장기 해결

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-n, m, k = map(int, input().split())
+chuck = list(map(int, input().split()))
+eungyu = list(map(int, input().split()))
 
-dohun = k*m
-hyeongjun = m
+cho = 0
+han = 1.5
 
-print(dohun + hyeongjun)
+point = [13, 7, 5, 3, 3, 2]
+
+for i in range(6):
+    cho += chuck[i] * point[i]
+    han += eungyu[i] * point[i]
+
+if cho > han:
+    print('cocjr0208')
+else:
+    print('ekwoo')
