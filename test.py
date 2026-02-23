64e0d92f98c3f6084e780135081ee5fdfee76329
7ee5u61n <le2471@gmail.com>
2026-02-23 11:17:16 +0900

    팩토리얼 진법 해결

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
-chuck = list(map(int, input().split()))
-eungyu = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-cho = 0
-han = 1.5
+def fact(n):
+    value = 0
+    lenth = len(str(n))
+    for i in range(lenth):
+        temp = 1
+        for j in range(1, lenth-i+1):
+             temp *= j
+        temp *= int(str(n)[i])
+        value += temp
+    return value
 
-point = [13, 7, 5, 3, 3, 2]
-
-for i in range(6):
-    cho += chuck[i] * point[i]
-    han += eungyu[i] * point[i]
-
-if cho > han:
-    print('cocjr0208')
-else:
-    print('ekwoo')
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(fact(n))
