8ece7dc09a6c50d9a7fe6f38a2343df2b3cccd61
7ee5u61n <le2471@gmail.com>
2026-02-24 23:58:23 +0900

    복호화 해결

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-import sys
-input = sys.stdin.readline
-
-def fact(n):
-    value = 0
-    lenth = len(str(n))
-    for i in range(lenth):
-        temp = 1
-        for j in range(1, lenth-i+1):
-             temp *= j
-        temp *= int(str(n)[i])
-        value += temp
-    return value
-
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    print(fact(n))
+T = int(input())
+for _ in range(T):
+    frequency = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 
+                 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 
+                 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    cipher = str(input())
+    for char in cipher:
+        if char in frequency:
+            frequency[char] += 1
+    sorted_freq = sorted(frequency.items(), key=lambda item: item[1], reverse=True)
+    if sorted_freq[0][1] == sorted_freq[1][1]:
+        print('?')
+    else:
+        print(sorted_freq[0][0])
