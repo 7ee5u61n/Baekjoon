6af5f416caaec0b23d45e7670a5f05fe6769996f
7ee5u61n <le2471@gmail.com>
2026-02-27 10:53:29 +0900

    연대 다음 고대 해결

--- 칵테일 도수.py
+++ 칵테일 도수.py
@@ -1,16 +1,15 @@
-print('재료 개수')
 n = int(input())
+yonsei = 0
+korea = 0
 
-numerator = 0
-denominator = 0
 for i in range(n):
-    print('용량, 도수')
-    a, b = map(int, input().split())
-    numerator += (a*b)
-    denominator += a
+    university = input()
+    if university == "yonsei":
+        yonsei = i
+    elif university == "korea":
+        korea = i
 
-volume = round(numerator/denominator, 1)
-alcohol = round(volume*0.01*denominator*0.785)
-
-print('도수: %, 알코올량: g')
-print(volume, alcohol)
+if yonsei < korea:
+    print("Yonsei Won!")
+else:
+    print("Yonsei Lost...")
