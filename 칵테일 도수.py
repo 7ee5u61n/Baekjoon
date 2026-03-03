34a0928e0ee3abc09b14ced947984f54d881c4b1
7ee5u61n <le2471@gmail.com>
2026-03-03 22:16:24 +0900

    관리자는 누구? 해결

--- 칵테일 도수.py
+++ 칵테일 도수.py
@@ -1,11 +1,13 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-T = int(input())
+candidate = [['PROBRAIN', 0], ['GROW', 0], ['ARGOS', 0], 
+             ['ADMIN', 0], ['ANT', 0], ['MOTION', 0], 
+             ['SPG', 0], ['COMON', 0], ['ALMIGHTY', 0]]
 
-for _ in range(T):
-    n = int(input())
-    if n ** 0.5 == int(n ** 0.5):
-        print(1)
-    else:
-        print(0)
+for i in range(9):
+    solve = list(map(int, input().split()))
+    candidate[i][1] = max(solve)
+
+candidate.sort(key=lambda x: (-x[1]))
+
+print(candidate[0][0])
