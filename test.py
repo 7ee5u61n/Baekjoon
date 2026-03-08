edff68caa08df401522895fc7d3d08bb8988dfac
7ee5u61n <le2471@gmail.com>
2026-03-08 23:48:28 +0900

    유림이와 하람이의 두쫀쿠 대작전 해결

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-m = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
-result = 0
-for i in range(n):
-    b, l, s = map(float, input().split())
-    b = int(b)
-    s = int(s)
-    if s >= 17 and l >= 2.0:
-        result += m[b]
-
-print(result)
+dubai = 900+60+600+170+160+110
+print(n//dubai)
