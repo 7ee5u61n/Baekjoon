7760d0aa572865da1009126ec39b057aeed9ea69
7ee5u61n <le2471@gmail.com>
2026-03-15 15:00:06 +0900

    추석은 언제나 좋아 해결

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-y, c, p = map(int, input().split())
-
-result = min(y, c//2, p)
-
-print(result)
+n = int(input())
+if n >= 1000000:
+    print(int(n*0.20), int(n*0.80))
+elif n >= 500000:
+    print(int(n*0.15), int(n*0.85))
+elif n >= 100000:
+    print(int(n*0.10), int(n*0.90))
+else:
+    print(int(n*0.05), int(n*0.95))
