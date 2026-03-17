f804789b5d74bf06a36f3020f554564a8611a3a5
7ee5u61n <le2471@gmail.com>
2026-03-17 16:01:10 +0900

    모르고리즘 회장 정하기 해결

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-q = [0] * 5
+candidate = []
 for _ in range(n):
-    x, y = map(int, input().split())
-    if x > 0 and y > 0:
-        q[0] += 1
-    elif x < 0 and y > 0:
-        q[1] += 1
-    elif x < 0 and y < 0:
-        q[2] += 1
-    elif x > 0 and y < 0:
-        q[3] += 1
-    else:
-        q[4] += 1
+    name = str(input())
+    if len(name) == 3:
+        candidate.append(name)
 
-for i in range(4):
-    print(f"Q{i + 1}: {q[i]}")
-print(f"AXIS: {q[4]}")
+candidate.sort()
+print(candidate[0])
