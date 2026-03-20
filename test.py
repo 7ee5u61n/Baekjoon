982b7fcbc0f38bf3ecaf094248e251f0df7f6791
7ee5u61n <le2471@gmail.com>
2026-03-21 00:07:24 +0900

    감마선을 맞은 컴퓨터 해결

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-n = int(input())
-
-result = 0
-for i in range(n):
-    x = input().split('-')
-    if int(x[1]) <= 90:
-        result += 1
+result = ''
+for _ in range(15):
+    pixel = list(input().split())
+    for i in range(15):
+        if pixel[i] == 'w':
+            result = 'chunbae'
+        elif pixel[i] == 'b':
+            result = 'nabi'
+        elif pixel[i] == 'g':
+            result = 'yeongcheol'
 
 print(result)
