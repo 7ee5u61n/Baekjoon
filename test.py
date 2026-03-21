8389dcfe0a516f8a4f43cce1e38b882758b6271c
7ee5u61n <le2471@gmail.com>
2026-03-21 20:13:11 +0900

    사격 내기 해결

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-result = ''
-for _ in range(15):
-    pixel = list(input().split())
-    for i in range(15):
-        if pixel[i] == 'w':
-            result = 'chunbae'
-        elif pixel[i] == 'b':
-            result = 'nabi'
-        elif pixel[i] == 'g':
-            result = 'yeongcheol'
+a, b = map(int, input().split())
 
-print(result)
+print(a^b)
