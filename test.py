a52af4ee8f614510172998da0ef8c21f7fbba4b1
7ee5u61n <le2471@gmail.com>
2026-03-22 21:42:32 +0900

    추첨을 통해 커피를 받자 해결

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-a, b = map(int, input().split())
+point = [100, 100, 200, 200, 300, 300, 400, 400, 500]
+solve = list(map(int, input().split()))
 
-print(a^b)
+isHacker = False
+total = 0
+for i in range(9):
+    if solve[i] > point[i]:
+        isHacker = True
+        break
+    total += solve[i]
+
+if isHacker:
+    print("hacker")
+else:
+    if total >= 100:
+        print("draw")
+    else:
+        print("none")
