a3c8e92b917b933c9bbf2c0ecde9acd882f9d9fc
7ee5u61n <le2471@gmail.com>
2026-03-28 00:03:13 +0900

    찬반투표 해결

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 n = int(input())
 
-result = 0
-for i in range(1, 501):
-    for j in range(i, 501):
-        if i**2 + n == j**2:
-            result += 1
+arr = [0]*3
+vote = list(map(int, input().split()))
 
-print(result)
+for i in vote:
+    if i == 1:
+        arr[0] += 1
+    elif i == -1:
+        arr[1] += 1
+    else:
+        arr[2] += 1
+
+if arr[2] >= n/2:
+    print('INVALID')
+else:
+    if arr[0] > arr[1]:
+        print('APPROVED')
+    else:
+        print('REJECTED')
