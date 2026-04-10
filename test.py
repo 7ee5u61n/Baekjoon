29dd8aa9b08c5968e2bbc8e4cebd6e8c9acbf8dd
7ee5u61n <le2471@gmail.com>
2026-04-10 13:24:47 +0900

    꼬리를 무는 숫자 나열 해결

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-T = int(input())
+a, b = map(int, input().split())
 
-for _ in range(T):
-    n = int(input())
-    total = 0
-    gpa = 0
-    for _ in range(n):
-        c, g = map(float, input().split())
-        total += c
-        gpa += c * g
+a -= 1
+b -= 1
 
-    print(int(total), round(gpa / total, 1))
+row = abs(a//4 - b//4)
+col = abs(a%4 - b%4)
+
+print(row + col)
