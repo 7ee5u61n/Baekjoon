7ddae37d5b6d2026a74e33cbc37c763b40fcd34b
7ee5u61n <le2471@gmail.com>
2026-04-14 23:36:56 +0900

    헤일스톤 수열 해결

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-while True:
-    s = str(input())
-    if s == '*':
-        break
-    
-    alphabet = set()
-    for i in s:
-        if i.isalpha():
-            alphabet.add(i)
-    
-    if len(alphabet) == 26:
-        print('Y')
-    else:
-        print('N')
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    mx = n
+    while n != 1:
+        if n % 2:
+            n = 3 * n + 1
+        else:
+            n //= 2
+        mx = max(mx, n)
+    print(mx)
