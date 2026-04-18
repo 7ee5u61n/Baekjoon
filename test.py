db3aec0b056d02806ff078bf7695af2fff107fbe
7ee5u61n <le2471@gmail.com>
2026-04-18 16:50:12 +0900

    팰린드롬 척화비 해결

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    w = 0
-    for k in range(1, n+1):
-        w += k*(sum(i for i in range(1, k+2)))
-    print(w)
+n = int(input())
+
+for _ in range(n):
+    print('a', end='')
