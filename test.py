fbb5cabd576ef17c52b2929563306aa8e9341c29
7ee5u61n <le2471@gmail.com>
2026-04-20 13:42:04 +0900

    임스의 일일 퀘스트 해결

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-k = int(input())
-d1, d2 = map(int, input().split())
+def quest(level, data):
+    result = []
+    for lv1, lv2, lv3 in data:
+        if level < lv1:
+            result.append(0)
+        elif level >= lv3:
+            result.append(100)
+        elif level >= lv2:
+            result.append(300)
+        else:
+            result.append(500)
+    return result
 
-h = k**2-((d1-d2)/2)**2
-print(h)
+arcane = [[200, 210, 220], [210, 220, 225], [220, 225, 230], [225, 230, 235], [230, 235, 245], [235, 245, 250]]
+authentic = [[260, 265, 270], [265, 270, 275], [270, 275, 280], [275, 280, 285], [280, 285, 290], [285, 290, 295], [290, 295, 300]]
+
+n = int(input())
+
+print(*quest(n, arcane))
+print(*quest(n, authentic))
