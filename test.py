d75d8b863dbfb0434cdae0ea68bc7cf4a8fb81de
7ee5u61n <le2471@gmail.com>
2026-04-21 12:42:47 +0900

    주사위 해결

--- test.py
+++ test.py
@@ -1,20 +1,10 @@
-def quest(level, data):
-    result = []
-    for lv1, lv2, lv3 in data:
-        if level < lv1:
-            result.append(0)
-        elif level >= lv3:
-            result.append(100)
-        elif level >= lv2:
-            result.append(300)
-        else:
-            result.append(500)
-    return result
+s1, s2, s3 = map(int, input().split())
 
-arcane = [[200, 210, 220], [210, 220, 225], [220, 225, 230], [225, 230, 235], [230, 235, 245], [235, 245, 250]]
-authentic = [[260, 265, 270], [265, 270, 275], [270, 275, 280], [275, 280, 285], [280, 285, 290], [285, 290, 295], [290, 295, 300]]
+sm = [0]*(s1+s2+s3+1)
 
-n = int(input())
+for i in range(1, s1+1):
+    for j in range(1, s2+1):
+        for k in range(1, s3+1):
+            sm[i+j+k] += 1
 
-print(*quest(n, arcane))
-print(*quest(n, authentic))
+print(sm.index(max(sm)))
