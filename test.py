f5a0c1f0c1cca60c586dd32b63269190ad0b1566
7ee5u61n <le2471@gmail.com>
2026-04-22 23:05:04 +0900

    줄 세기 해결

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-s1, s2, s3 = map(int, input().split())
+result = 0
+while True:
+    try:
+        s = str(input())
+        result += 1
+    except EOFError:
+        break
 
-sm = [0]*(s1+s2+s3+1)
-
-for i in range(1, s1+1):
-    for j in range(1, s2+1):
-        for k in range(1, s3+1):
-            sm[i+j+k] += 1
-
-print(sm.index(max(sm)))
+print(result)
